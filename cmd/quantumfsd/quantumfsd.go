0638a451ab11e7b2fcb5d623bb93f48473ad0de9
Travis Brown <travisb@aristanetworks.com>
2016-02-17 14:58:45 -0800

    Add basic description
    Change-Id: Id486ed2d2a2b241756c9125d284f75b758278eb7

--- cmd/quantumfsd/quantumfsd.go
+++ cmd/quantumfsd/quantumfsd.go
@@ -1,6 +1,7 @@
 // Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
 // Arista Networks, Inc. Confidential and Proprietary.
 
+// quantumfsd is the central daemon of the filesystem
 package main
 
 func main() {
