a920374a313b0e55fb133c617fd50aca7aebd0f5
Travis Brown <travisb@aristanetworks.com>
2016-02-24 16:47:04 -0800

    Return . and .. in workspace namespace listing
    Change-Id: I607a5c53a962749d4b66e47d3c4a0bf7c7f4d1d9

--- cmd/quantumfsd/workspacelisting.go
+++ cmd/quantumfsd/workspacelisting.go
@@ -5,6 +5,7 @@
 // directory hierarchy.
 package main
 
+import "fmt"
 import "time"
 
 import "github.com/hanwen/go-fuse/fuse"
@@ -20,26 +21,93 @@ type NamespaceList struct {
 	InodeCommon
 }
 
-func (nsl *NamespaceList) GetAttr(out *fuse.AttrOut) (result fuse.Status) {
-	out.AttrValid = 1
-	out.AttrValid = 1
-	out.Attr.Ino = nsl.InodeCommon.id
-	out.Attr.Size = 4096
-	out.Attr.Blocks = 1
+func (nsl *NamespaceList) GetAttr(out *fuse.AttrOut) fuse.Status {
+	out.AttrValid = config.cacheTimeSeconds
+	out.AttrValidNsec = config.cacheTimeNsecs
+
+	namespaceListFillAttr(&out.Attr, nsl.InodeCommon.id)
+	return fuse.OK
+}
+
+func namespaceListFillAttr(attr *fuse.Attr, inodeNum uint64) {
+	attr.Ino = inodeNum
+	attr.Size = 4096
+	attr.Blocks = 1
 
 	now := time.Now()
-	out.Attr.Atime = uint64(now.Unix())
-	out.Attr.Atimensec = uint32(now.Nanosecond())
-	out.Attr.Mtime = uint64(now.Unix())
-	out.Attr.Mtimensec = uint32(now.Nanosecond())
+	attr.Atime = uint64(now.Unix())
+	attr.Atimensec = uint32(now.Nanosecond())
+	attr.Mtime = uint64(now.Unix())
+	attr.Mtimensec = uint32(now.Nanosecond())
 
-	out.Attr.Ctime = 1
-	out.Attr.Ctimensec = 1
-	out.Attr.Mode = 0555 | fuse.S_IFDIR
-	out.Attr.Nlink = 1024
-	out.Attr.Owner.Uid = 0
-	out.Attr.Owner.Gid = 0
-	out.Attr.Blksize = 4096
+	attr.Ctime = 1
+	attr.Ctimensec = 1
+	attr.Mode = 0555 | fuse.S_IFDIR
+	attr.Nlink = 1024
+	attr.Owner.Uid = 0
+	attr.Owner.Gid = 0
+	attr.Blksize = 4096
+}
+
+func (nsl *NamespaceList) OpenDir(flags uint32, mode uint32, out *fuse.OpenOut) (result fuse.Status) {
+	ns := newNamespaceSnapshot()
+	globalQfs.setFileHandle(ns.FileHandleCommon.id, ns)
+	out.Fh = ns.FileHandleCommon.id
+	out.OpenFlags = 0
+
+	return fuse.OK
+}
+
+func newNamespaceSnapshot() *namespaceSnapshot {
+	ns := namespaceSnapshot{
+		FileHandleCommon: FileHandleCommon{
+			id:       fuse.FUSE_ROOT_ID,
+			inodeNum: fuse.FUSE_ROOT_ID,
+		},
+	}
+
+	return &ns
+}
+
+type namespaceSnapshot struct {
+	FileHandleCommon
+}
+
+func (ns *namespaceSnapshot) ReadDirPlus(input *fuse.ReadIn, out *fuse.DirEntryList) fuse.Status {
+	fmt.Println("ReadDirPlus", input, out)
+	if input.Offset != 0 {
+		return fuse.OK
+	}
+
+	// Add .
+	entry := fuse.DirEntry{Mode: fuse.S_IFDIR, Name: "."}
+	details, _ := out.AddDirLookupEntry(entry)
+	if details == nil {
+		return fuse.OK
+	}
+
+	details.NodeId = ns.FileHandleCommon.inodeNum
+	details.Generation = 1
+	details.EntryValid = config.cacheTimeSeconds
+	details.EntryValidNsec = config.cacheTimeNsecs
+	details.AttrValid = config.cacheTimeSeconds
+	details.AttrValidNsec = config.cacheTimeNsecs
+	namespaceListFillAttr(&details.Attr, ns.FileHandleCommon.inodeNum)
+
+	// Add .., even though this will be overwritten
+	entry = fuse.DirEntry{Mode: fuse.S_IFDIR, Name: ".."}
+	details, _ = out.AddDirLookupEntry(entry)
+	if details == nil {
+		return fuse.OK
+	}
+
+	details.NodeId = ns.FileHandleCommon.inodeNum
+	details.Generation = 1
+	details.EntryValid = config.cacheTimeSeconds
+	details.EntryValidNsec = config.cacheTimeNsecs
+	details.AttrValid = config.cacheTimeSeconds
+	details.AttrValidNsec = config.cacheTimeNsecs
+	namespaceListFillAttr(&details.Attr, ns.FileHandleCommon.inodeNum)
 
 	return fuse.OK
 }
