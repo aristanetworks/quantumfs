da6350cdd6c8174a4c8b09d9215724ca820cf5c2
Travis Brown <travisb@aristanetworks.com>
2016-02-24 16:44:03 -0800

    Add cache time config options
    Change-Id: I0cc65f9b8545483e2fedc097a26d3da40af3f6c2

--- cmd/quantumfsd/config.go
+++ cmd/quantumfsd/config.go
@@ -14,16 +14,23 @@ type QuantumFsConfig struct {
 	cachePath string
 	cacheSize uint64
 	mountPath string
+
+	// How long the kernel is allowed to cache values
+	cacheTimeSeconds uint64
+	cacheTimeNsecs   uint32
 }
 
 var cacheSizeString string
+var cacheTimeNsecs uint
 var config QuantumFsConfig
 
 func init() {
 	const (
-		defaultCachePath = "/dev/shmem"
-		defaultCacheSize = "8G"
-		defaultMountPath = "/mnt/quantumfs"
+		defaultCachePath        = "/dev/shmem"
+		defaultCacheSize        = "8G"
+		defaultMountPath        = "/mnt/quantumfs"
+		defaultCacheTimeSeconds = 1
+		defaultCacheTimeNsecs   = 0
 	)
 
 	flag.StringVar(&config.cachePath, "cachePath", defaultCachePath,
@@ -33,6 +40,10 @@ func init() {
 		"Size of the local cache, e.g. 8G or 512M")
 	flag.StringVar(&config.mountPath, "mountpath", defaultMountPath,
 		"Path to mount quantumfs at")
+	flag.Uint64Var(&config.cacheTimeSeconds, "cacheTimeSeconds", defaultCacheTimeSeconds,
+		"Number of seconds the kernel will cache response data")
+	flag.UintVar(&cacheTimeNsecs, "cacheTimeNsecs", defaultCacheTimeNsecs,
+		"Number of nanoseconds the kernel will cache response data")
 }
 
 // Process the command arguments. Will show the command usage if no arguments are
@@ -47,5 +58,6 @@ func processArgs() {
 	} else {
 		config.cacheSize = cacheSize
 	}
+	config.cacheTimeNsecs = (uint32)(cacheTimeNsecs)
 
 }
