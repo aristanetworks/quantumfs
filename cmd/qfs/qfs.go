74376fb140ec8f09b83a370416eedd82e4e18789
Travis Brown <travisb@aristanetworks.com>
2016-03-01 13:28:01 -0800

    Create structure for qfs branch command
    Change-Id: I05644dc3f5b55f18d40d8cc1f70f4b0189d55329

--- cmd/qfs/qfs.go
+++ cmd/qfs/qfs.go
@@ -5,8 +5,18 @@
 // special operations not available through normal POSIX interfaces.
 package main
 
-import "fmt"
 import "flag"
+import "fmt"
+import "os"
+
+import "arista.com/quantumfs"
+
+// Various exist reasons, will be returned to the shell as an exit code
+const (
+	exitOk      = iota
+	exitBadCmd  = iota
+	exitBadArgs = iota
+)
 
 func main() {
 	flag.Parse()
@@ -18,5 +28,37 @@ func main() {
 		fmt.Println("  branch <workspaceO> <workspaceN>")
 		fmt.Println("         - create a new workspaceN which is a copy of workspaceO")
 		fmt.Println("           as of this point in time")
+		os.Exit(exitBadCmd)
+	}
+
+	cmd := flag.Arg(0)
+	switch cmd {
+	default:
+		fmt.Printf("Unknown command \"%s\"\n", cmd)
+		os.Exit(exitBadCmd)
+
+	case "branch":
+		branch()
+	}
+}
+
+// Implement the branch command, which takes a workspace at the current spot, and
+// creates a new workspace with the given name which is identical to the first
+// workspace as of right now.
+func branch() {
+	if flag.NArg() != 3 {
+		fmt.Println("Too few arguments for branch command")
+		os.Exit(exitBadArgs)
+	}
+
+	src := flag.Arg(1)
+	dst := flag.Arg(2)
+
+	fmt.Printf("Branching workspace \"%s\" into \"%s\"\n", src, dst)
+	err := quantumfs.Branch(src, dst)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(exitBadArgs)
 	}
 }
---
+++ cmds.go
@@ -0,0 +1,25 @@
+// Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+package quantumfs
+
+import "fmt"
+import "strings"
+
+// This file contains all the functions which are used by qfs (and other
+// applications) to perform special quantumfs operations. Primarily this is done by
+// marhalling the arguments and passing them to quantumfsd for processing, then
+// interpretting the results.
+
+// Branch the src workspace into a new workspace called dst.
+func Branch(src string, dst string) error {
+	if slashes := strings.Count(src, "/"); slashes != 1 {
+		return fmt.Errorf("\"%s\" must contain precisely one \"/\"\n", src)
+	}
+
+	if slashes := strings.Count(dst, "/"); slashes != 1 {
+		return fmt.Errorf("\"%s\" must contain precisely one \"/\"\n", dst)
+	}
+
+	return nil
+}
