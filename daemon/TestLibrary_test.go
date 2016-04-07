7983a91be0663dd3afa83dfd68c849e52f4de627
Travis Brown <travisb@arista.com>
2016-04-07 16:32:52 -0700

    Add testlibrary with helpers to launch quantumfs

---
+++ daemon/TestLibrary_test.go
@@ -0,0 +1,89 @@
+// Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+package daemon
+
+// Test library
+
+import "io/ioutil"
+import "os"
+import "testing"
+
+import "arista.com/quantumfs"
+import "arista.com/quantumfs/processlocal"
+
+import "github.com/hanwen/go-fuse/fuse"
+
+// startTest is a helper which configures the testing environment
+func startTest(t *testing.T) testHelper {
+	t.Parallel()
+	return testHelper{t: t}
+}
+
+// endTest cleans up the testing environment after the test has finished
+func (th *testHelper) endTest() {
+	if th.server != nil {
+		if err := th.server.Unmount(); err != nil {
+			th.t.Fatalf("Failed to unmount quantumfs instance: %v", err)
+		}
+	}
+
+	if th.mountPath != "" {
+		if err := os.RemoveAll(th.mountPath); err != nil {
+			th.t.Fatalf("Failed to cleanup temporary mount point: %v", err)
+		}
+	}
+}
+
+// This helper is more of a namespacing mechanism than a coherent object
+type testHelper struct {
+	t         *testing.T
+	qfs       *QuantumFs
+	mountPath string
+	server    *fuse.Server
+}
+
+func (th *testHelper) defaultConfig() QuantumFsConfig {
+	mountPath, err := ioutil.TempDir("", "quantumfsTest")
+	if err != nil {
+		th.t.Fatalf("Unable to create temporary mount point: %v", err)
+	}
+
+	th.mountPath = mountPath
+	th.t.Log("Using mountpath", mountPath)
+
+	config := QuantumFsConfig{
+		CachePath:        "",
+		CacheSize:        1 * 1024 * 1024,
+		CacheTimeSeconds: 1,
+		CacheTimeNsecs:   0,
+		MountPath:        mountPath,
+		WorkspaceDB:      processlocal.NewWorkspaceDB(),
+		DurableStore:     processlocal.NewDataStore(),
+	}
+	return config
+}
+
+func (th *testHelper) startDefaultQuantumFs() {
+	config := th.defaultConfig()
+	th.startQuantumFs(config)
+}
+
+func (th *testHelper) startQuantumFs(config QuantumFsConfig) {
+	var mountOptions = fuse.MountOptions{
+		AllowOther:    true,
+		MaxBackground: 1024,
+		MaxWrite:      quantumfs.MaxBlockSize,
+		FsName:        "cluster",
+		Name:          config.MountPath,
+	}
+
+	quantumfs := NewQuantumFs(config)
+	server, err := fuse.NewServer(quantumfs, config.MountPath, &mountOptions)
+	if err != nil {
+		th.t.Fatalf("Failed to create quantumfs instance: %v", err)
+	}
+
+	th.server = server
+	go server.Serve()
+}
