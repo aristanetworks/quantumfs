f0e4133dea636b219707f2f89c02629be4706b31
Travis Brown <travisb@arista.com>
2016-04-25 14:54:15 -0700

    Unsatisfied test of File dirtying

--- daemon/File_test.go
+++ daemon/File_test.go
@@ -37,3 +37,46 @@ func TestFileCreation_test(t *testing.T) {
 
 	test.endTest()
 }
+
+func TestFileDescriptorDirtying_test(t *testing.T) {
+	test := startTest(t)
+	test.startDefaultQuantumFs()
+
+	// Create a file and determine its inode numbers
+	workspace := quantumfs.NullNamespaceName + "/" + quantumfs.NullWorkspaceName
+	testFilename := workspace + "/" + "test"
+	fd, err := syscall.Creat(test.relPath(testFilename), 0124)
+	test.assert(err != nil, "Error creating file: %v", err)
+	var stat syscall.Stat_t
+	err = syscall.Stat(test.relPath(testFilename), &stat)
+	test.assert(err == nil, "Error stat'ind test file: %v", err)
+	test.assert(stat.Ino >= quantumfs.InodeIdReservedEnd,
+		"File had reserved inode number %d", stat.Ino)
+
+	// Find the matching FileHandle
+	descriptors := test.fileDescriptorFromInodeNum(stat.Ino)
+	test.assert(len(descriptors) == 1, "Incorrect number of fds found 1 != %d",
+		len(descriptors))
+	fileDescriptor := descriptors[0]
+	file := fileDescriptor.file
+
+	// Save the workspace rootId, change the File key, simulating changing the
+	// data, then mark the matching FileDescriptor dirty. This should trigger a
+	// refresh up the hierarchy and, because we currently do not support delayed
+	// syncing, change the workspace rootId and mark the fileDescriptor clean.
+	oldRootId := test.workspaceRootId(quantumfs.NullNamespaceName,
+		quantumfs.NullWorkspaceName)
+
+	file.key.Key[1]++
+	fileDescriptor.dirty(test.newCtx())
+
+	newRootId := test.workspaceRootId(quantumfs.NullNamespaceName,
+		quantumfs.NullWorkspaceName)
+
+	test.assert(oldRootId != newRootId, "Workspace rootId didn't change")
+	test.assert(!file.dirty_, "FileDescriptor not cleaned after"+
+		"change")
+
+	syscall.Close(fd)
+	test.endTest()
+}
