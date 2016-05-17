a141b4eae45e23655991f600cd7c77403ce9f0af
Travis Brown <travisb@arista.com>
2016-05-17 15:48:51 -0700

    Abort filesystem if unmount fails
    Sometimes unmounting may fail to happen cleanly, usually because
    something is still using the filesystem. In these situations
    forcefully abort the filesystem and then attempt to unmount the
    filesystem again.

--- daemon/TestLibrary_test.go
+++ daemon/TestLibrary_test.go
@@ -77,6 +77,11 @@ func (th *testHelper) execute(test quantumFsTest) {
 }
 
 func abortFuse(th *testHelper) {
+	if th.fuseConnection == 0 {
+		// Nothing to abort
+		return
+	}
+
 	// Forcefully abort the filesystem so it can be unmounted
 	th.t.Logf("Aborting FUSE connection %d", th.fuseConnection)
 	path := fmt.Sprintf("%s/connections/%d/abort", fusectlPath,
@@ -104,12 +109,18 @@ func (th *testHelper) endTest() {
 	}
 
 	if th.server != nil {
-		if exception != nil && th.fuseConnection != 0 {
+		if exception != nil {
 			abortFuse(th)
 		}
 
 		if err := th.server.Unmount(); err != nil {
 			th.t.Fatalf("Failed to unmount quantumfs instance: %v", err)
+			abortFuse(th)
+
+			if err := th.server.Unmount(); err != nil {
+				th.t.Fatalf("Failed to unmount quantumfs instance "+
+					"after aborting: %v", err)
+			}
 		}
 	}
 
