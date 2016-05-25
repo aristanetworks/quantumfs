f067c08ae3fbf902e447c6cca7407c7f4f6a207f
Peter Neufeld <pwn@arista.com>
2016-05-25 13:26:19 -0700

    Remove accidentally committed code

--- daemon/TestLibrary_test.go
+++ daemon/TestLibrary_test.go
@@ -10,7 +10,6 @@ import "fmt"
 import "io/ioutil"
 import "os"
 import "runtime"
-import "runtime/debug"
 import "strings"
 import "strconv"
 import "sync"
@@ -352,8 +351,7 @@ func (th *testHelper) newCtx() *ctx {
 // message
 func (th *testHelper) assert(condition bool, format string, args ...interface{}) {
 	if !condition {
-		msg := fmt.Sprintf("%s-------\n", debug.Stack())
-		msg += fmt.Sprintf(format, args)
+		msg := fmt.Sprintf(format, args)
 		panic(msg)
 	}
 }
