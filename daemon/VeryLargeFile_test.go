8775202991f8de997fff0113f30771bdf9d83104
Peter Neufeld <pwn@arista.com>
2016-07-05 16:38:40 -0700

    Minor Very large file test refactor and addition of other file type conversion tests

--- daemon/VeryLargeFile_test.go
+++ daemon/VeryLargeFile_test.go
@@ -10,55 +10,74 @@ import "io/ioutil"
 import "os"
 import "testing"
 
+func (test *testHelper) runConvertFrom(fromFileSize uint64) {
+	test.startDefaultQuantumFs()
+
+	workspace := test.nullWorkspace()
+	testFilename := workspace + "/test"
+
+	data := genFibonacci(512 * 1024)
+	err := printToFile(testFilename, string(data))
+	test.assert(err == nil,
+		"Error writing 0.5MB fibonacci to new fd: %v", err)
+	// Make this the file size desired (and hence type)
+	err = os.Truncate(testFilename, int64(fromFileSize))
+	test.assert(err == nil, "Unable to truncate expand file to %d", fromFileSize)
+
+	// 49GB sparse file incoming
+	newLen := 49 * 1024 * 1024 * 1024
+	os.Truncate(testFilename, int64(newLen))
+	test.assert(test.fileSize(testFilename) == int64(newLen),
+		"Truncation expansion failed")
+
+	// Then append fib *again* to the end of it
+	err = printToFile(testFilename, string(data))
+	test.assert(err == nil,
+		"Error writing 0.5MB fibonacci to existing fd: %v", err)
+	test.assert(test.fileSize(testFilename) == int64(newLen+len(data)),
+		"Post truncation expansion write failed, %d",
+		test.fileSize(testFilename))
+
+	// Read our fib back
+	fd, fdErr := os.OpenFile(testFilename, os.O_RDONLY, 0777)
+	test.assert(fdErr == nil, "Unable to open file for RDONLY")
+	// Try to read more than should exist
+	startData := test.readTo(fd, 0, len(data)+1000)
+	err = fd.Close()
+	test.assert(err == nil, "Unable to close file")
+	test.assert(bytes.Equal(data, startData[:len(data)]),
+		"Data not preserved in small to very large expansion")
+	test.assert(bytes.Equal(startData[len(data):], make([]byte, 1000)),
+		"Data hole not created in small to very large file expand")
+
+	// Now check the fib at the end of the file
+	fd, fdErr = os.OpenFile(testFilename, os.O_RDONLY, 0777)
+	test.assert(fdErr == nil, "Unable to open file for RDONLY")
+	// Try to read more than should exist
+	endData := test.readTo(fd, newLen-1000, len(data)+2000)
+	err = fd.Close()
+	test.assert(err == nil, "Unable to close file")
+	test.assert(bytes.Equal(endData[:1000], make([]byte, 1000)),
+		"Data hole not in small to very large file expansion")
+	test.assert(bytes.Equal(data, endData[1000:]),
+		"Data entry error after small to very large expansion")
+}
+
 func TestSmallConvert_test(t *testing.T) {
 	runTest(t, func(test *testHelper) {
-		test.startDefaultQuantumFs()
+		test.runConvertFrom(512 * 1024)
+	})
+}
 
-		workspace := test.nullWorkspace()
-		testFilename := workspace + "/test"
+func TestMedConvert_test(t *testing.T) {
+	runTest(t, func(test *testHelper) {
+		test.runConvertFrom(7 * 1024 * 1024)
+	})
+}
 
-		data := genFibonacci(512 * 1024)
-		err := printToFile(testFilename, string(data))
-		test.assert(err == nil,
-			"Error writing 0.5MB fibonacci to new fd: %v", err)
-
-		// 49GB sparse file incoming
-		newLen := 49 * 1024 * 1024 * 1024
-		os.Truncate(testFilename, int64(newLen))
-		test.assert(test.fileSize(testFilename) == int64(newLen),
-			"Truncation expansion failed")
-
-		// Then append fib *again* to the end of it
-		err = printToFile(testFilename, string(data))
-		test.assert(err == nil,
-			"Error writing 0.5MB fibonacci to existing fd: %v", err)
-		test.assert(test.fileSize(testFilename) == int64(newLen+len(data)),
-			"Post truncation expansion write failed, %d",
-			test.fileSize(testFilename))
-
-		// Read our fib back
-		fd, fdErr := os.OpenFile(testFilename, os.O_RDONLY, 0777)
-		test.assert(fdErr == nil, "Unable to open file for RDONLY")
-		// Try to read more than should exist
-		startData := test.readTo(fd, 0, len(data)+1000)
-		err = fd.Close()
-		test.assert(err == nil, "Unable to close file")
-		test.assert(bytes.Equal(data, startData[:len(data)]),
-			"Data not preserved in small to very large expansion")
-		test.assert(bytes.Equal(startData[len(data):], make([]byte, 1000)),
-			"Data hole not created in small to very large file expand")
-
-		// Now check the fib at the end of the file
-		fd, fdErr = os.OpenFile(testFilename, os.O_RDONLY, 0777)
-		test.assert(fdErr == nil, "Unable to open file for RDONLY")
-		// Try to read more than should exist
-		endData := test.readTo(fd, newLen-1000, len(data)+2000)
-		err = fd.Close()
-		test.assert(err == nil, "Unable to close file")
-		test.assert(bytes.Equal(endData[:1000], make([]byte, 1000)),
-			"Data hole not in small to very large file expansion")
-		test.assert(bytes.Equal(data, endData[1000:]),
-			"Data entry error after small to very large expansion")
+func TestLargeConvert_test(t *testing.T) {
+	runTest(t, func(test *testHelper) {
+		test.runConvertFrom(44 * 1024 * 1024)
 	})
 }
 
