14bfa745cdba7e15068df51cbee27d00104bb55c
Travis Brown <travisb@arista.com>
2016-07-20 10:43:47 -0700

    Factor mknod creation test out

--- daemon/Special_test.go
+++ daemon/Special_test.go
@@ -8,76 +8,49 @@ package daemon
 import "syscall"
 import "testing"
 
+func specialCreate(test *testHelper, filetype uint32) {
+	test.startDefaultQuantumFs()
+
+	workspace := test.newWorkspace()
+	testFilename := workspace + "/" + "test"
+	err := syscall.Mknod(testFilename, filetype|syscall.S_IRWXU,
+		0x12345678)
+	test.assert(err == nil, "Error creating node: %v", err)
+
+	confirm := func(workspace string) {
+		var stat syscall.Stat_t
+		err = syscall.Stat(testFilename, &stat)
+		test.assert(err == nil, "Error stat'ing test file: %v", err)
+		test.assert(stat.Size == 0, "Incorrect Size: %d", stat.Size)
+		test.assert(stat.Nlink == 1, "Incorrect Nlink: %d",
+			stat.Nlink)
+		test.assert(stat.Rdev == 0x12345678,
+			"Node rdev incorrect %x", stat.Rdev)
+
+		var expectedPermissions uint32
+		expectedPermissions |= filetype
+		expectedPermissions |= syscall.S_IRWXU | syscall.S_IRWXG |
+			syscall.S_IRWXO
+		test.assert(stat.Mode == expectedPermissions,
+			"File permissions incorrect. Expected %x got %x",
+			expectedPermissions, stat.Mode)
+	}
+
+	confirm(workspace)
+
+	// Branch and confirm everything is still correct
+	workspace = test.absPath(test.branchWorkspace(workspace))
+	confirm(workspace)
+}
+
 func TestBlockDevCreation_test(t *testing.T) {
 	runTest(t, func(test *testHelper) {
-		test.startDefaultQuantumFs()
-
-		workspace := test.newWorkspace()
-		testFilename := workspace + "/" + "test"
-		err := syscall.Mknod(testFilename, syscall.S_IFBLK|syscall.S_IRWXU,
-			0x12345678)
-		test.assert(err == nil, "Error creating block device: %v", err)
-
-		confirm := func(workspace string) {
-			var stat syscall.Stat_t
-			err = syscall.Stat(testFilename, &stat)
-			test.assert(err == nil, "Error stat'ing test file: %v", err)
-			test.assert(stat.Size == 0, "Incorrect Size: %d", stat.Size)
-			test.assert(stat.Nlink == 1, "Incorrect Nlink: %d",
-				stat.Nlink)
-			test.assert(stat.Rdev == 0x12345678,
-				"Block rdev incorrect %x", stat.Rdev)
-
-			var expectedPermissions uint32
-			expectedPermissions |= syscall.S_IFBLK
-			expectedPermissions |= syscall.S_IRWXU | syscall.S_IRWXG |
-				syscall.S_IRWXO
-			test.assert(stat.Mode == expectedPermissions,
-				"File permissions incorrect. Expected %x got %x",
-				expectedPermissions, stat.Mode)
-		}
-
-		confirm(workspace)
-
-		// Branch and confirm everything is still correct
-		workspace = test.absPath(test.branchWorkspace(workspace))
-		confirm(workspace)
+		specialCreate(test, syscall.S_IFBLK)
 	})
 }
 
 func TestCharDevCreation_test(t *testing.T) {
 	runTest(t, func(test *testHelper) {
-		test.startDefaultQuantumFs()
-
-		workspace := test.newWorkspace()
-		testFilename := workspace + "/" + "test"
-		err := syscall.Mknod(testFilename, syscall.S_IFCHR|syscall.S_IRWXU,
-			0x12345678)
-		test.assert(err == nil, "Error creating char device: %v", err)
-
-		confirm := func(workspace string) {
-			var stat syscall.Stat_t
-			err = syscall.Stat(testFilename, &stat)
-			test.assert(err == nil, "Error stat'ing test file: %v", err)
-			test.assert(stat.Size == 0, "Incorrect Size: %d", stat.Size)
-			test.assert(stat.Nlink == 1, "Incorrect Nlink: %d",
-				stat.Nlink)
-			test.assert(stat.Rdev == 0x12345678,
-				"Block rdev incorrect %x", stat.Rdev)
-
-			var expectedPermissions uint32
-			expectedPermissions |= syscall.S_IFCHR
-			expectedPermissions |= syscall.S_IRWXU | syscall.S_IRWXG |
-				syscall.S_IRWXO
-			test.assert(stat.Mode == expectedPermissions,
-				"File permissions incorrect. Expected %x got %x",
-				expectedPermissions, stat.Mode)
-		}
-
-		confirm(workspace)
-
-		// Branch and confirm everything is still correct
-		workspace = test.absPath(test.branchWorkspace(workspace))
-		confirm(workspace)
+		specialCreate(test, syscall.S_IFCHR)
 	})
 }
