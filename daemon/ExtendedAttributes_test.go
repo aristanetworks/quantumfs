0b0cbdc1f3caeb19c1a2c08e39067d0f11870292
Travis Brown <travisb@arista.com>
2016-08-11 13:59:04 -0700

    Test XAttr listing

--- daemon/ExtendedAttributes_test.go
+++ daemon/ExtendedAttributes_test.go
@@ -6,7 +6,9 @@ package daemon
 // Test the various operations on extended attributes
 
 import "bytes"
+import "fmt"
 import "os"
+import "strings"
 import "syscall"
 import "testing"
 
@@ -56,5 +58,51 @@ func TestExtendedAttrReadWrite(t *testing.T) {
 			"data XAttr size incorrect: %d", size)
 		test.assert(bytes.Equal(data[:size], attrDataData),
 			"Didn't get the same data back '%s' '%s'", data, attrDataData)
+	})
+}
+
+func TestExtendedAttrList(t *testing.T) {
+	runTest(t, func(test *testHelper) {
+		test.startDefaultQuantumFs()
+
+		workspace := test.newWorkspace()
+		testFilename := workspace + "/" + "test"
+		fd, err := os.Create(testFilename)
+		test.assert(err == nil, "Error creating test file: %v", err)
+		fd.Close()
+
+		// Confirm no attributes
+		data := make([]byte, 64000)
+		size, err := syscall.Listxattr(testFilename, data)
+		test.assert(err == nil, "Error listing XAttr: %v", err)
+		test.assert(size == 0, "Unexpected XAttr, size %d", size)
+
+		// Add a bunch of attributes
+		const N = 100
+		for i := 0; i < N; i++ {
+			attrName := fmt.Sprintf("user.attr%d", i)
+			err = syscall.Setxattr(testFilename, attrName, []byte{}, 0)
+			test.assert(err == nil, "Error setting XAttr %s: %v",
+				attrName, err)
+		}
+
+		// Confirm they are all there
+		data = make([]byte, 64000)
+		size, err = syscall.Listxattr(testFilename, data)
+		test.assert(err == nil, "Error listing XAttr: %v", err)
+		test.assert(size > 0, "Expected XAttr, but didn't find any")
+		test.assert(size <= len(data), "XAttr names overflowed buffer")
+
+		data = data[:size]
+		names := bytes.Split(data, []byte("\x00"))
+		names = names[:len(names)-1] // Remove empty last element
+		test.assert(len(names) == N, "Fewer XAttr than expected: %d != %d",
+			len(names), N)
+
+		for _, nameBytes := range names {
+			name := string(nameBytes)
+			test.assert(strings.HasPrefix(name, "user.attr"),
+				"Incorrect XAttr name %s", name)
+		}
 	})
 }
