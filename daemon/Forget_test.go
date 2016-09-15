bf9c5f8e2ad8fb898a4486ab513bd5acafb596ba
Peter Neufeld <pwn@arista.com>
2016-09-15 13:20:43 -0700

    Formatting changes

--- daemon/Forget_test.go
+++ daemon/Forget_test.go
@@ -14,7 +14,6 @@ import "testing"
 
 import "github.com/aristanetworks/quantumfs/qlog"
 
-
 func TestForget_test(t *testing.T) {
 	runTest(t, func(test *testHelper) {
 		test.startDefaultQuantumFs()
@@ -25,13 +24,13 @@ func TestForget_test(t *testing.T) {
 		data := genData(255)
 		// Generate a bunch of files
 		for i := 0; i < numFiles; i++ {
-			err := printToFile(workspace + "/file" + strconv.Itoa(i),
+			err := printToFile(workspace+"/file"+strconv.Itoa(i),
 				string(data))
 			test.assert(err == nil, "Error creating small file")
 		}
 
 		// Now force the kernel to drop all cached inodes
-		cmd := exec.Command("mount", "-i", "-oremount", test.tempDir +
+		cmd := exec.Command("mount", "-i", "-oremount", test.tempDir+
 			"/mnt")
 		errorStr, err := cmd.CombinedOutput()
 		test.assert(err == nil, "Unable to force vfs to drop dentry cache")
