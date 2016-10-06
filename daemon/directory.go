53c3bb08514026fe67521de681cc59dd3ddae072
Travis Brown <travisb@arista.com>
2016-10-06 13:46:54 -0700

    Have directorySnapshot update its children list on rewinddir
    The definition of rewinddir() is that it moves the directory iterator
    to the beginning of the directory and invalidates any cached data.
    Thus files which were added or deleted between the opendir() and
    rewinddir() must be visible after rewinddir().

--- daemon/directory.go
+++ daemon/directory.go
@@ -413,6 +413,15 @@ func (dir *Directory) Open(c *ctx, flags uint32, mode uint32,
 func (dir *Directory) OpenDir(c *ctx, flags uint32, mode uint32,
 	out *fuse.OpenOut) fuse.Status {
 
+	ds := newDirectorySnapshot(c, dir.self.(directorySnapshotSource))
+	c.qfs.setFileHandle(c, ds.FileHandleCommon.id, ds)
+	out.Fh = uint64(ds.FileHandleCommon.id)
+	out.OpenFlags = 0
+
+	return fuse.OK
+}
+
+func (dir *Directory) getChildSnapshot(c *ctx) []directoryContents {
 	defer dir.RLock().RUnlock()
 
 	children := make([]directoryContents, 0, dir.dirChildren.count())
@@ -433,12 +442,7 @@ func (dir *Directory) OpenDir(c *ctx, flags uint32, mode uint32,
 		children = append(children, entryInfo)
 	}
 
-	ds := newDirectorySnapshot(c, children, dir.InodeCommon.id, dir.treeLock())
-	c.qfs.setFileHandle(c, ds.FileHandleCommon.id, ds)
-	out.Fh = uint64(ds.FileHandleCommon.id)
-	out.OpenFlags = 0
-
-	return fuse.OK
+	return children
 }
 
 func (dir *Directory) create_(c *ctx, name string, mode uint32, umask uint32,
@@ -1169,16 +1173,21 @@ type directoryContents struct {
 	attr     fuse.Attr
 }
 
-func newDirectorySnapshot(c *ctx, children []directoryContents,
-	inodeNum InodeId, treeLock *sync.RWMutex) *directorySnapshot {
+type directorySnapshotSource interface {
+	getChildSnapshot(c *ctx) []directoryContents
+	inodeNum() InodeId
+	treeLock() *sync.RWMutex
+}
+
+func newDirectorySnapshot(c *ctx, src directorySnapshotSource) *directorySnapshot {
 
 	ds := directorySnapshot{
 		FileHandleCommon: FileHandleCommon{
 			id:        c.qfs.newFileHandleId(),
-			inodeNum:  inodeNum,
-			treeLock_: treeLock,
+			inodeNum:  src.inodeNum(),
+			treeLock_: src.treeLock(),
 		},
-		children: children,
+		src: src,
 	}
 
 	assert(ds.treeLock() != nil, "directorySnapshot treeLock nil at init")
@@ -1189,6 +1198,7 @@ func newDirectorySnapshot(c *ctx, children []directoryContents,
 type directorySnapshot struct {
 	FileHandleCommon
 	children []directoryContents
+	src      directorySnapshotSource
 }
 
 func (ds *directorySnapshot) ReadDirPlus(c *ctx, input *fuse.ReadIn,
@@ -1196,6 +1206,11 @@ func (ds *directorySnapshot) ReadDirPlus(c *ctx, input *fuse.ReadIn,
 
 	c.vlog("ReadDirPlus directorySnapshot")
 	offset := input.Offset
+
+	if offset == 0 {
+		c.dlog("Refreshing child list")
+		ds.children = ds.src.getChildSnapshot(c)
+	}
 
 	// Add .
 	if offset == 0 {
@@ -1264,13 +1279,4 @@ func (ds *directorySnapshot) Write(c *ctx, offset uint64, size uint32, flags uin
 
 func (ds *directorySnapshot) Sync(c *ctx) fuse.Status {
 	return fuse.OK
-}
-
-func (ds *directorySnapshot) appendApi() {
-	api := directoryContents{
-		filename: quantumfs.ApiPath,
-		fuseType: fuse.S_IFREG,
-	}
-	fillApiAttr(&api.attr)
-	ds.children = append(ds.children, api)
 }
