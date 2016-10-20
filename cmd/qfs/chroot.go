876e7f80005115de73620af48515f94e61773243
bernardy@arista.com <bernardy@arista.com>
2016-10-20 11:41:28 -0700

    fixed coding sytle issues

--- cmd/qfs/chroot.go
+++ cmd/qfs/chroot.go
@@ -5,7 +5,6 @@
 // the current workspace root becomes the filesystem root
 package main
 
-import "bytes"
 import "fmt"
 import "io/ioutil"
 import "os"
@@ -37,12 +36,12 @@ func findWorkspaceRoot() (string, error) {
 	}
 
 	dirs := strings.Split(wd, "/")
+
 	for len(dirs) > 1 {
 		rootdir := strings.Join(dirs, "/")
 		toolDir := rootdir + ArtoolsDir
 
-		toolInfo, err := os.Stat(toolDir)
-		if err == nil && toolInfo.IsDir() {
+		if toolInfo, err := os.Stat(toolDir); err == nil && toolInfo.IsDir() {
 			return rootdir, nil
 		}
 
@@ -52,7 +51,9 @@ func findWorkspaceRoot() (string, error) {
 	return "", fmt.Errorf("Invalid path for chroot")
 }
 
-// this function creates dst given the type of src if dst does not exist
+// This function creates dst given the type of src if dst does not exist.
+// It returns true if dst exists and is the same type as src, or dst is
+// successfully created, otherwise returns false.
 func makedest(src, dst string) bool {
 	srcInfo, err := os.Stat(src)
 	if err != nil {
@@ -60,26 +61,27 @@ func makedest(src, dst string) bool {
 	}
 
 	dstInfo, err := os.Stat(dst)
-	if err == nil {
-		if srcInfo.IsDir() == dstInfo.IsDir() {
+	if err != nil {
+		return false
+	}
+
+	if srcInfo.IsDir() == dstInfo.IsDir() {
+		return true
+	}
+
+	if srcInfo.IsDir() {
+		if err := os.Mkdir(dst, 0666); err != nil {
+			return false
+		} else {
 			return true
 		}
-	}
-	if srcInfo.IsDir() {
-		mkdir_err := os.Mkdir(dst, 0666)
-		if mkdir_err != nil {
-			fmt.Println("Error creating directory ", dst)
-			return false
-		}
-		return true
 	} else {
-		fd, create_err := os.Create(dst)
-		if create_err != nil {
-			fmt.Println("Error creating file ", dst)
+		if fd, err := os.Create(dst); err != nil {
 			return false
+		} else {
+			fd.Close()
+			return true
 		}
-		fd.Close()
-		return true
 	}
 }
 
@@ -87,13 +89,11 @@ func makedest(src, dst string) bool {
 func homedirs() []string {
 	homes := make([]string, 0)
 
-	arastra, err := user.Lookup("arastra")
-	if err == nil {
+	if arastra, err := user.Lookup("arastra"); err == nil {
 		homes = append(homes, arastra.HomeDir)
 	}
 
-	current, err := user.Current()
-	if err == nil && current.Username != "root" {
+	if current, err := user.Current(); err == nil && current.Username != "root" {
 		homes = append(homes, current.HomeDir)
 	}
 
@@ -111,6 +111,7 @@ func processArchitecture(arch string) (string, error) {
 	case "x86_64":
 		return "x86_64", nil
 	}
+
 	return "", fmt.Errorf("Unrecognized architecture")
 }
 
@@ -122,33 +123,38 @@ func getArchitecture(rootdir string) (string, error) {
 	}
 
 	platStr := string(platform[:len(platform)-1])
+
 	return processArchitecture(platStr)
 }
 
 // test whether the netns server is already running
 func serverRunning(svrName string) bool {
 	cmdServerRun := exec.Command("netns", "-q", svrName)
-	err := cmdServerRun.Run()
-	if err == nil {
+
+	if err := cmdServerRun.Run(); err == nil {
 		return true
-	} else {
-		return false
 	}
+
+	return false
 }
 
 // login the netns server and open a new login shell, which is not
 // expected to return
 func netnsLogin(rootdir string, svrName string, root bool) error {
 	var err error
+
 	env := os.Environ()
 	env = append(env, "A4_CHROOT="+rootdir)
+
 	if root {
 		args := []string{sudo, netns, svrName, sh, "-l", "-c",
 			"\"$@\"", bash, bash}
+
 		err = syscall.Exec(sudo, args, env)
 	} else {
 		args := []string{netns, svrName, sh, "-l", "-c",
 			"\"$@\"", bash, bash}
+
 		err = syscall.Exec(netns, args, env)
 	}
 
@@ -156,39 +162,43 @@ func netnsLogin(rootdir string, svrName string, root bool) error {
 }
 
 func chrootInNsd(rootdir string, svrName string) error {
-	cmdBindMountRoot := fmt.Sprintf("%s %s -n --rbind %s %s;",
-		sudo, mount, rootdir, rootdir)
+	bindmountRoot := fmt.Sprintf("%s %s -n --rbind %s %s;", sudo, mount,
+		rootdir, rootdir)
 
 	dstDev := rootdir + "/dev"
 	makedest("/dev", dstDev)
-	cmdMountDev := fmt.Sprintf("%s %s -n -t tmpfs none %s;",
+
+	mountDev := fmt.Sprintf("%s %s -n -t tmpfs none %s;",
 		sudo, mount, dstDev)
-	cmdCopyDev := fmt.Sprintf("%s %s -ax /dev/. %s;", sudo, cp, dstDev)
+
+	copyDev := fmt.Sprintf("%s %s -ax /dev/. %s;", sudo, cp, dstDev)
 
 	dstVar := rootdir + "/var/run/netns"
-	err := os.MkdirAll(dstVar, 0666)
-	if err != nil {
+	if err := os.MkdirAll(dstVar, 0666); err != nil {
 		return err
 	}
-	cmdMountVar := fmt.Sprintf("%s %s -n -t tmpfs tmpfs %s;",
-		sudo, mount, dstVar)
 
-	var otherBindMounts string
+	mountVar := fmt.Sprintf("%s %s -n -t tmpfs tmpfs %s;", sudo, mount, dstVar)
+
 	paths := []string{"/proc", "/selinux", "/sys", "/dev/pts", "/tmp/.X11-unix",
 		"/tmp/ArosTest.SimulatedDut", "/mnt/quantumfs"}
 	homes := homedirs()
 	paths = append(paths, homes...)
+
+	var bindmountOther string
 	for i := 0; i < len(paths); i++ {
 		src := paths[i]
 		dst := rootdir + paths[i]
 		if !makedest(src, dst) {
 			continue
 		}
-		otherBindMounts = otherBindMounts +
+
+		bindmountOther = bindmountOther +
 			fmt.Sprintf("%s %s -n --bind %s %s;", sudo, mount, src, dst)
 	}
-	prechrootCmd := cmdBindMountRoot + cmdMountDev +
-		cmdCopyDev + cmdMountVar + otherBindMounts
+
+	prechrootCmd := bindmountRoot + mountDev + copyDev +
+		mountVar + bindmountOther
 
 	archString, err := getArchitecture(rootdir)
 	if err != nil {
@@ -198,10 +208,7 @@ func chrootInNsd(rootdir string, svrName string) error {
 	cmdNetnsd := exec.Command(sudo, setarch, archString, netnsd,
 		"-d", "--no-netns-env", "-f", "m", "--chroot="+rootdir,
 		"--pre-chroot-cmd="+prechrootCmd, svrName)
-	var cmdNetnsdError bytes.Buffer
-	cmdNetnsd.Stderr = &cmdNetnsdError
-	err = cmdNetnsd.Run()
-	if err != nil {
+	if err = cmdNetnsd.Run(); err != nil {
 		return err
 	}
 
@@ -214,20 +221,20 @@ func chroot() {
 		fmt.Println(err.Error())
 		return
 	}
+
 	svrName := rootdir + "/chroot"
 
 	if !serverRunning(svrName) {
-		err = chrootInNsd(rootdir, svrName)
-		if err != nil {
+		if err = chrootInNsd(rootdir, svrName); err != nil {
 			fmt.Println(err.Error())
 			return
 		}
 	}
 
-	err = netnsLogin(rootdir, svrName, false)
-	if err != nil {
+	if err = netnsLogin(rootdir, svrName, false); err != nil {
 		fmt.Println(err.Error())
 		return
 	}
+
 	return
 }
