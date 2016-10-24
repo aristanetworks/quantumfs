af93d2662e23f6db68d3b5b711a37de2562993f2
Travis Brown <travisb@arista.com>
2016-10-24 15:04:26 -0700

    Mark new link uninstantiated

--- daemon/directory_DOWN.go
+++ daemon/directory_DOWN.go
@@ -40,6 +40,7 @@ func (dir *Directory) link_DOWN(c *ctx, srcInode Inode, newName string,
 		newRecord)
 
 	dir.self.dirty(c)
+	c.qfs.addUninstantiated(c, []InodeId{inodeNum}, dir)
 
 	return fuse.OK
 }
