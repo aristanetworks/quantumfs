074c127fc96e64bbdb117f7f3103ac7665c62817
Yang <yang@arista.com>
2016-10-28 17:56:42 -0700

    format checking

--- daemon/Api_test.go
+++ daemon/Api_test.go
@@ -72,132 +72,132 @@ func TestApiClearAccessList(t *testing.T) {
 }
 
 func TestApiDuplicateObject(t *testing.T) {
-	runTest(t, func(test *testHelper) {
-		test.startDefaultQuantumFs()
-		api := test.getApi()
-
-		// First branch the source workspace
+        runTest(t, func(test *testHelper) {
+                test.startDefaultQuantumFs()
+                api := test.getApi()
+ 
+                // First branch the source workspace
 		workspace := test.newWorkspace()
-		src := test.relPath(workspace)
+                src := test.relPath(workspace)
 		dst := "apitest/duplicate"
 		err := api.Branch(src, dst)
 		test.assert(err == nil, "Failed to branch workspace: %v", err)
+ 
+                dirName := workspace + "/test/a"
+                dirName1 := dirName +"/b"
+                testFilename := dirName1 + "/test"
+                linkFilename := workspace + "/link"
 
-		dirName := workspace + "/test/a"
-		dirName1 := dirName + "/b"
-		testFilename := dirName1 + "/test"
-		linkFilename := workspace + "/link"
+                err = os.MkdirAll(dirName1, 0124)
+                test.assert(err == nil, "Error creating directories: %v", err)
 
-		err = os.MkdirAll(dirName1, 0124)
-		test.assert(err == nil, "Error creating directories: %v", err)
+                fd, err := syscall.Creat(testFilename, 0777)
+                syscall.Close(fd)
+                test.assert(err == nil, "Error creating a small file: %v", err)
+ 
+                err = syscall.Symlink(testFilename, linkFilename)
+                test.assert(err == nil, "Error creating a symlink: %v", err)
 
-		fd, err := syscall.Creat(testFilename, 0777)
-		syscall.Close(fd)
-		test.assert(err == nil, "Error creating a small file: %v", err)
+                test.syncAllWorkspaces()
 
-		err = syscall.Symlink(testFilename, linkFilename)
-		test.assert(err == nil, "Error creating a symlink: %v", err)
+                // get the key from a file
+                keyF := make([]byte, TypeKeyLength)
+                sz, err := syscall.Getxattr(testFilename, XAttrTypeKey, keyF)
+                test.assert(err == nil && sz == TypeKeyLength,
+                "Error getting the typeKeyF: %v with a size of %d", err, sz)
 
-		test.syncAllWorkspaces()
+                // get the key from a directory
+                keyD := make([]byte, TypeKeyLength)
+                sz, err = syscall.Getxattr(dirName1, XAttrTypeKey, keyD)
+                test.assert(err == nil && sz == TypeKeyLength,
+                "Error getting the typeKeyD: %v with a size of %d", err, sz)
 
-		// get the key from a file
-		keyF := make([]byte, TypeKeyLength)
-		sz, err := syscall.Getxattr(testFilename, XAttrTypeKey, keyF)
-		test.assert(err == nil && sz == TypeKeyLength,
-			"Error getting the typeKeyF: %v with a size of %d", err, sz)
+                // get the key from a Symlink
+                keyS := make([]byte, TypeKeyLength)
+                sz, err, keyS = lGetXattr(linkFilename, XAttrTypeKey, TypeKeyLength)
+                test.assert(err == nil && sz == TypeKeyLength,
+                "Error getting the typeKeyS: %v with a size of %d", err, sz)
 
-		// get the key from a directory
-		keyD := make([]byte, TypeKeyLength)
-		sz, err = syscall.Getxattr(dirName1, XAttrTypeKey, keyD)
-		test.assert(err == nil && sz == TypeKeyLength,
-			"Error getting the typeKeyD: %v with a size of %d", err, sz)
+                dirNameD := test.absPath( dst+ "/test/a")
+                err = os.MkdirAll(dirNameD, 0124)
+                test.assert(err == nil, "Error creating target directories: %v", err)
 
-		// get the key from a Symlink
-		keyS := make([]byte, TypeKeyLength)
-		sz, err, keyS = lGetXattr(linkFilename, XAttrTypeKey, TypeKeyLength)
-		test.assert(err == nil && sz == TypeKeyLength,
-			"Error getting the typeKeyS: %v with a size of %d", err, sz)
+                // Ensure the workspace root cannot be duplicated
+                err = api.DuplicateObject(dst, keyF, 0124, 0022, 0, 0, 0)
+                test.assert(err != nil,
+                        "Error duplicating workspace root")
 
-		dirNameD := test.absPath(dst + "/test/a")
-		err = os.MkdirAll(dirNameD, 0124)
-		test.assert(err == nil, "Error creating target directories: %v", err)
+                // Ensure the non-existing intermediate Inode not be created
+                err = api.DuplicateObject(dst + "/nonExist/b", keyF,
+                                                0124, 0022, 0, 0, 0)
+                test.assert(err != nil, 
+                        "Error creating non-existing Inode")
 
-		// Ensure the workspace root cannot be duplicated
-		err = api.DuplicateObject(dst, keyF, 0124, 0022, 0, 0, 0)
-		test.assert(err != nil,
-			"Error duplicating workspace root")
+                // Ensure the target node does not exist
+                err = api.DuplicateObject(dst + "/test/a", keyF,
+                                                0124, 0022, 0, 0, 0)
+                test.assert(err != nil,
+                        "Error having the target node already")
+ 
+                // Duplicate the file in the given path
+                err = api.DuplicateObject(dst + "/test/a/file", keyF,
+                                                0124, 0022, 0, 0, 0)
+                test.assert(err == nil,
+                        "Error duplicating a file to workspaceT: %v", err)
 
-		// Ensure the non-existing intermediate Inode not be created
-		err = api.DuplicateObject(dst+"/nonExist/b", keyF,
-			0124, 0022, 0, 0, 0)
-		test.assert(err != nil,
-			"Error creating non-existing Inode")
+                var stat syscall.Stat_t
+                err = syscall.Stat(test.absPath(dst + "/test/a/file"), &stat)
+                test.assert(err == nil, "Error get status of a file: %v", err)
+ 
+                // check the mode of file
+                var expectedPermissions uint32
+                expectedPermissions |= syscall.S_IFREG
+                expectedPermissions |= syscall.S_IXUSR | syscall.S_IROTH
+                test.assert(stat.Mode == expectedPermissions,
+                        "File permissions incorrect. Expected %x got %x",
+                        expectedPermissions, stat.Mode)
 
-		// Ensure the target node does not exist
-		err = api.DuplicateObject(dst+"/test/a", keyF,
-			0124, 0022, 0, 0, 0)
-		test.assert(err != nil,
-			"Error having the target node already")
+                //Duplicate the directory in the given path
+                err = api.DuplicateObject(dst + "/test/a/dirtest", keyD,
+                                                0124, 0022, 0, 0, 0)
+                test.assert(err == nil,
+                        "Error duplicating a directory to workspaceT: %v", err)
 
-		// Duplicate the file in the given path
-		err = api.DuplicateObject(dst+"/test/a/file", keyF,
-			0124, 0022, 0, 0, 0)
-		test.assert(err == nil,
-			"Error duplicating a file to workspaceT: %v", err)
+                err = syscall.Stat(test.absPath(dst + "/test/a/dirtest"), &stat)
+                test.assert(err == nil, "Error getting status of directory: %v",
+                        err)
 
-		var stat syscall.Stat_t
-		err = syscall.Stat(test.absPath(dst+"/test/a/file"), &stat)
-		test.assert(err == nil, "Error get status of a file: %v", err)
+                // check the mode of directory
+                expectedPermissions = syscall.S_IFDIR
+                expectedPermissions |= syscall.S_IXUSR | syscall.S_IROTH
+                test.assert(stat.Mode == expectedPermissions,
+                        "Directory permissions incorrect. Expected %x got %x",
+                        expectedPermissions, stat.Mode)
 
-		// check the mode of file
-		var expectedPermissions uint32
-		expectedPermissions |= syscall.S_IFREG
-		expectedPermissions |= syscall.S_IXUSR | syscall.S_IROTH
-		test.assert(stat.Mode == expectedPermissions,
-			"File permissions incorrect. Expected %x got %x",
-			expectedPermissions, stat.Mode)
+                // testing the file inside of the directory
+                err = syscall.Stat(test.absPath(dst + "/test/a/dirtest/test"), &stat)
+                test.assert(err == nil, "Error getting status of child file: %v",
+                         err)
 
-		//Duplicate the directory in the given path
-		err = api.DuplicateObject(dst+"/test/a/dirtest", keyD,
-			0124, 0022, 0, 0, 0)
-		test.assert(err == nil,
-			"Error duplicating a directory to workspaceT: %v", err)
+                // check the child is a file
+                expectedPermissions = syscall.S_IFREG
+                test.assert(stat.Mode & expectedPermissions != 0,
+                        "Directory permissions incorrect. Expected %x got %x",
+                        expectedPermissions, stat.Mode)
 
-		err = syscall.Stat(test.absPath(dst+"/test/a/dirtest"), &stat)
-		test.assert(err == nil, "Error getting status of directory: %v",
-			err)
+                // Ensure the symlink in the given path
+                err = api.DuplicateObject(dst + "/symlink", keyS,
+                                                0777, 0777, 0, 0, 0)
+                test.assert(err == nil,
+                        "Error duplicating a symlink to workspace: %v", err)
 
-		// check the mode of directory
-		expectedPermissions = syscall.S_IFDIR
-		expectedPermissions |= syscall.S_IXUSR | syscall.S_IROTH
-		test.assert(stat.Mode == expectedPermissions,
-			"Directory permissions incorrect. Expected %x got %x",
-			expectedPermissions, stat.Mode)
-
-		// testing the file inside of the directory
-		err = syscall.Stat(test.absPath(dst+"/test/a/dirtest/test"), &stat)
-		test.assert(err == nil, "Error getting status of child file: %v",
-			err)
-
-		// check the child is a file
-		expectedPermissions = syscall.S_IFREG
-		test.assert(stat.Mode&expectedPermissions != 0,
-			"Directory permissions incorrect. Expected %x got %x",
-			expectedPermissions, stat.Mode)
-
-		// Ensure the symlink in the given path
-		err = api.DuplicateObject(dst+"/symlink", keyS,
-			0777, 0777, 0, 0, 0)
-		test.assert(err == nil,
-			"Error duplicating a symlink to workspace: %v", err)
-
-		err = syscall.Lstat(test.absPath(dst+"/symlink"), &stat)
-		test.assert(err == nil,
-			"Error getting status of Symlink: %v", err)
-
-		expectedPermissions = syscall.S_IFLNK
-		test.assert(stat.Mode&expectedPermissions != 0,
-			"Symlink permissions incorrect. Expected %x got %x %d",
-			expectedPermissions, stat.Mode, stat.Size)
-	})
+                err = syscall.Lstat(test.absPath(dst + "/symlink"), &stat)
+                test.assert(err == nil,
+                        "Error getting status of Symlink: %v", err)
+ 
+                expectedPermissions = syscall.S_IFLNK
+                test.assert(stat.Mode & expectedPermissions != 0,
+                        "Symlink permissions incorrect. Expected %x got %x %d",
+                        expectedPermissions, stat.Mode, stat.Size)
+        })
 }
