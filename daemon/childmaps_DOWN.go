20e8599948777c685dec535c5560b46bb3687db4
Peter Neufeld <pwn@arista.com>
2017-01-09 18:31:13 -0800

    Added missing file

---
+++ daemon/childmaps_DOWN.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+package daemon
+
+// This is _DOWN counterpart to childmaps.go
+
+import "github.com/aristanetworks/quantumfs"
+import "github.com/hanwen/go-fuse/fuse"
+
+func (cmap *ChildMap) makeHardlink_DOWN(c *ctx, wsr *WorkspaceRoot,
+	childId InodeId) (copy DirectoryRecordIf, err fuse.Status) {
+
+	child, exists := cmap.childrenRecords[childId]
+	if !exists {
+		c.elog("No child record or %d in childmap", childId)
+		return nil, fuse.ENOENT
+	}
+
+	// If it's already a hardlink, great no more work is needed
+	if link, isLink := child.(*Hardlink); isLink {
+		recordCopy := *link
+		return &recordCopy, fuse.OK
+	}
+
+	// record must be a file type to be hardlinked
+	if child.Type() != quantumfs.ObjectTypeSmallFile &&
+		child.Type() != quantumfs.ObjectTypeMediumFile &&
+		child.Type() != quantumfs.ObjectTypeLargeFile &&
+		child.Type() != quantumfs.ObjectTypeVeryLargeFile {
+
+		c.dlog("Cannot hardlink %d - not a file", childId)
+		return nil, fuse.EINVAL
+	}
+
+	// If the file hasn't been synced already, then we can't link it yet
+	if _, exists = cmap.dirtyChildren[childId]; exists {
+		c.elog("makeHardlink called on dirty child %d", childId)
+		return nil, fuse.EIO
+	}
+
+	// It needs to become a hardlink now. Hand it off to wsr
+	newLink := wsr.newHardlink(c, childId, child)
+	cmap.childrenRecords[childId] = newLink
+	linkCopy := *newLink
+	return &linkCopy, fuse.OK
+}
