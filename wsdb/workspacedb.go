5043d2bba16a3a46a9639c3c842bbc857d8db0e6
Krishnanand Thommandra <kthommandra@arista.com>
2017-01-11 08:59:33 -0800

    Added WorkspaceDB package to break QFS dependency
    Currently this package is in ether repo but eventually it'll be
    moved to its own repo. This package defines the workspace DB interface
    and associated types that all workspace DB clients can use.
    
    patchset-2: incorporate review comments, fix lint warnings
    patchset-3: remove unnecessary package alias
    
    Change-Id: Idca910b7a0398f3154d47e6276ffa7b361c7e77a

---
+++ wsdb/workspacedb.go
@@ -0,0 +1,115 @@
+// Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+// Package wsdb is the interface to the Workspace Name database.
+package wsdb
+
+import "fmt"
+
+// ObjectKey is key used to access object in the cluster-wide
+// object store using Ether's BlobStore API
+type ObjectKey []byte
+
+// ErrCode is error code from workspace DB APIs
+type ErrCode int
+
+const (
+	// ErrReserved should not be used
+	ErrReserved ErrCode = iota
+	// ErrFatal means API has encountered a fatal error
+	ErrFatal = iota
+	// ErrWorkspaceExists means that the workspace already exists
+	ErrWorkspaceExists = iota
+	// ErrWorkspaceNotFound means that the workspace doesn't exist
+	ErrWorkspaceNotFound = iota
+
+	// ErrWorkspaceOutOfDate means the operation was based on
+	// out of date information. This error occurs only when
+	// concurrent updates to same workspace is supported
+	// from different cluster nodes
+	ErrWorkspaceOutOfDate = iota
+)
+
+// Error represents the error returned from workspace DB APIs
+type Error struct {
+	Code ErrCode // This can be used as sentinal value
+	Msg  string  // This is for humans
+}
+
+// NewError returns a new error
+func NewError(code ErrCode, msg string, args ...interface{}) error {
+	return &Error{Code: code, Msg: fmt.Sprintf(msg, args...)}
+}
+
+// ErrorCode returns a readable string for ErrCode
+func (err *Error) ErrorCode() string {
+	switch err.Code {
+	default:
+		return "Unknown wsdb error"
+	case ErrWorkspaceExists:
+		return "Workspace already exists"
+	case ErrWorkspaceNotFound:
+		return "Workspace not found"
+	case ErrFatal:
+		return "Fatal workspaceDB error"
+	case ErrWorkspaceOutOfDate:
+		return "Workspace changed remotely"
+	}
+}
+
+func (err *Error) Error() string {
+	return fmt.Sprintf("Error: %s: %s", err.ErrorCode(), err.Msg)
+}
+
+// WorkspaceDB provides a cluster-wide and consistent mapping between names and
+// ObjectKeys. Workspace names have two components and are represented as strings with
+// the format "<namespace>/<workspace>". <namespace> would often be a username or
+// something like abuild. <workspace> will be somewhat arbitrary.
+//
+// Most of the API is synchronous and consistent. The notable exceptions are the
+// calls to list the available namespaces and workspaces within a namespace. These
+// must respond without blocking from a local cache if possible, perfectly accurate
+// data is not required.
+//
+// Since workspaceDB is a cluster-wide datastore, apart from errors like
+// ErrWorkspaceNotFound, ErrWorkspaceExists, it is possible to get
+// ErrFatal due to unrecoverable cluster issues
+type WorkspaceDB interface {
+
+	// These methods need to be instant, but not necessarily completely up to
+	// date
+	NumNamespaces() (int, error)
+	NamespaceList() ([]string, error)
+	NumWorkspaces(namespace string) (int, error)
+	WorkspaceList(namespace string) ([]string, error)
+
+	// These methods need to be up to date
+	NamespaceExists(namespace string) (bool, error)
+	WorkspaceExists(namespace string, workspace string) (bool, error)
+	Workspace(namespace string, workspace string) (ObjectKey, error)
+
+	// These methods need to be atomic, but may retry internally
+
+	// BranchWorkspace branches srcNamespace/srcWorkspace to create
+	// dstNamespace/dstWorkspace
+	//
+	// Possible errors are:
+	//  ErrWorkspaceExists
+	//  ErrWorkspaceNotFound
+	BranchWorkspace(srcNamespace string, srcWorkspace string,
+		dstNamespace string, dstWorkspace string) error
+
+	// AdvanceWorkspace changes the workspace rootID. If the current rootID
+	// doesn't match what the client considers the current rootID then no changes
+	// are made and the current rootID is returned such that the client can merge
+	// the changes and retry.
+	//
+	// The return value is the new rootID.
+	//
+	// Possible errors are:
+	//  ErrWorkspaceNotFound
+	//  ErrWorkspaceOutOfDate: The workspace rootID was changed remotely so the local
+	//                         instance is out of date
+	AdvanceWorkspace(namespace string, workspace string,
+		currentRootID ObjectKey, newRootID ObjectKey) (ObjectKey, error)
+}
