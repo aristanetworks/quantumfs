c792072a47ff1b9349433d0dbd44efa4e9093d97
Travis Brown <travisb@arista.com>
2017-01-18 17:02:33 -0800

    Fix test for new asynchronous forget world

--- daemon/Forget_test.go
+++ daemon/Forget_test.go
@@ -235,6 +235,8 @@ func TestForgetMarking(t *testing.T) {
 		childIdA := test.getInodeNum(workspace + "/testdir/a")
 		childIdB := test.getInodeNum(workspace + "/testdir/b")
 
+		test.syncAllWorkspaces()
+
 		// We need to trigger, ourselves, the kind of Forget sequence where
 		// markings are necessary: parent, childA, then childB
 		parent := test.qfs.inodeNoInstantiate(&test.qfs.c, parentId)
@@ -251,6 +253,7 @@ func TestForgetMarking(t *testing.T) {
 
 		// Now start Forgetting
 		test.qfs.Forget(uint64(parentId), 1)
+		test.syncAllWorkspaces()
 
 		// Parent should still be loaded
 		parent = test.qfs.inodeNoInstantiate(&test.qfs.c, parentId)
@@ -259,6 +262,7 @@ func TestForgetMarking(t *testing.T) {
 
 		// Forget one child, not enough to forget the parent
 		test.qfs.Forget(uint64(childIdA), 1)
+		test.syncAllWorkspaces()
 
 		parent = test.qfs.inodeNoInstantiate(&test.qfs.c, parentId)
 		test.assert(parent != nil,
@@ -269,16 +273,17 @@ func TestForgetMarking(t *testing.T) {
 
 		// Now forget the last child, which should unload the parent also
 		test.qfs.Forget(uint64(childIdB), 1)
-
-		parent = test.qfs.inodeNoInstantiate(&test.qfs.c, parentId)
-		test.assert(parent == nil,
-			"Parent %d not forgotten when all children unloaded",
-			parentId)
+		test.syncAllWorkspaces()
 
 		childA = test.qfs.inodeNoInstantiate(&test.qfs.c, childIdA)
 		test.assert(childA == nil, "ChildA not forgotten when requested")
 
 		childB = test.qfs.inodeNoInstantiate(&test.qfs.c, childIdB)
 		test.assert(childB == nil, "ChildB not forgotten when requested")
+
+		parent = test.qfs.inodeNoInstantiate(&test.qfs.c, parentId)
+		test.assert(parent == nil,
+			"Parent %d not forgotten when all children unloaded",
+			parentId)
 	})
 }
