5bc6344b3923090fce0c599c37d1544a9ffe8160
Travis Brown <travisb@arista.com>
2017-01-24 14:04:58 -0800

    Delete empty file

--- daemon/workspaceroot_DOWN.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
-// Arista Networks, Inc. Confidential and Proprietary.
-
-package daemon
-
-// This is the _DOWN counterpart to workspaceroot.go
