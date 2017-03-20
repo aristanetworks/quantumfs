515a2c836a239a2155cbeac86fd75aa67d769462
Peter Neufeld <pwn@arista.com>
2017-03-20 09:45:31 -0700

    Review changes

--- datastore.go
+++ datastore.go
@@ -681,7 +681,13 @@ type DirectoryRecord interface {
 
 	EncodeExtendedKey() []byte
 
-	ShallowCopy() *DirectRecord
+	// returns a direct record copy, regardless of underlying class, causing
+	// all future changes to the copy to be unreflected in the original.
+	// Mainly used for reads
+	ShallowCopy() DirectoryRecord
+
+	// returns a real copy, which can result in future changes changing the
+	// original depending on the underlying class.
 	Clone() DirectoryRecord
 }
 
@@ -810,7 +816,7 @@ func (record *DirectRecord) EncodeExtendedKey() []byte {
 	return EncodeExtendedKey(record.ID(), record.Type(), record.Size())
 }
 
-func (record *DirectRecord) ShallowCopy() *DirectRecord {
+func (record *DirectRecord) ShallowCopy() DirectoryRecord {
 	newEntry := NewDirectRecord()
 	newEntry.SetNlinks(record.Nlinks())
 	newEntry.SetFilename(record.Filename())
