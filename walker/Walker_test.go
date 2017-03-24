e52eba26c14797a134d78e2b2d4275d9759e68ba
Sidhartha Agrawal <sid@arista.com>
2017-03-24 16:03:09 -0700

    Round 2 of review comments.
    Walker and its test cases are added to simple how test infra will work.
    I will be removing walker before commiting the change and send out a
    different review for walker.

--- walker/Walker_test.go
+++ walker/Walker_test.go
@@ -4,15 +4,84 @@
 package walker
 
 import "flag"
-import "fmt"
 import "os"
-import "runtime/debug"
 import "runtime"
-import "sync"
+import "strings"
 import "testing"
+import "time"
 
 import "github.com/aristanetworks/quantumfs/daemon"
+import "github.com/aristanetworks/quantumfs/qlog"
 import "github.com/aristanetworks/quantumfs/testutils"
+
+// This is the normal way to run tests in the most time efficient manner
+func runTest(t *testing.T, test quantumFsTest) {
+	t.Parallel()
+	runTestCommon(t, test, true)
+}
+
+func runTestCommon(t *testing.T, test quantumFsTest,
+	startDefaultQfs bool) {
+	// Since we grab the test name from the backtrace, it must always be an
+	// identical number of frames back to the name of the test. Otherwise
+	// multiple tests will end up using the same temporary directory and nothing
+	// will work.
+	//
+	// 2 <testname>
+	// 1 runTest
+	// 0 runTestCommon
+	testPc, _, _, _ := runtime.Caller(2)
+	testName := runtime.FuncForPC(testPc).Name()
+	lastSlash := strings.LastIndex(testName, "/")
+	testName = testName[lastSlash+1:]
+	cachePath := daemon.TestRunDir + "/" + testName
+
+	th := &testHelper{
+		TestHelper: daemon.TestHelper{
+			TestHelper: testutils.TestHelper{
+				T:          t,
+				TestName:   testName,
+				TestResult: make(chan string, 2), // must be buffered
+				StartTime:  time.Now(),
+				CachePath:  cachePath,
+				Logger: qlog.NewQlogExt(cachePath+"/ramfs",
+					60*10000*24, daemon.NoStdOut),
+			},
+		},
+	}
+
+	th.CreateTestDirs()
+	defer th.EndTest()
+
+	if startDefaultQfs {
+		th.StartDefaultQuantumFs()
+	}
+
+	th.Log("Finished test preamble, starting test proper")
+	go th.Execute(th.testHelperUpcast(test))
+
+	testResult := th.WaitForResult()
+
+	if !th.ShouldFail && testResult != "" {
+		th.Log("ERROR: Test failed unexpectedly:\n%s\n", testResult)
+	} else if th.ShouldFail && testResult == "" {
+		th.Log("ERROR: Test is expected to fail, but didn't")
+	}
+}
+
+type testHelper struct {
+	daemon.TestHelper
+}
+
+type quantumFsTest func(test *testHelper)
+
+func (th *testHelper) testHelperUpcast(
+	testFn func(test *testHelper)) testutils.QuantumFsTest {
+
+	return func(test testutils.TestArg) {
+		testFn(th)
+	}
+}
 
 // TODO(sid)
 // Test Walk of Dir
