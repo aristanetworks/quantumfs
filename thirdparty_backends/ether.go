824cfd12240e9ea1c21b836a142d15093a32759e
Krishnanand Thommandra <kthommandra@arista.com>
2017-03-27 05:17:56 -0700

    Undo the ether adapter changes
    To be submitted separately

--- thirdparty_backends/ether.go
+++ thirdparty_backends/ether.go
@@ -67,15 +67,7 @@ func (ebt *EtherBlobStoreTranslator) Get(c *quantumfs.Ctx, key quantumfs.ObjectK
 func (ebt *EtherBlobStoreTranslator) Set(c *quantumfs.Ctx, key quantumfs.ObjectKey,
 	buf quantumfs.Buffer) error {
 
-	_, err := ebt.blobstore.Metadata(key.String())
-	if err != nil {
-		bE, _ := err.(*blobstore.Error)
-		// TODO(krishna): check TTL too
-		if bE.Code == blobstore.ErrKeyNotFound {
-			return ebt.blobstore.Insert(key.String(), buf.Get(), nil)
-		}
-	}
-	return err
+	return ebt.blobstore.Insert(key.String(), buf.Get(), nil)
 }
 
 type EtherWsdbTranslator struct {
