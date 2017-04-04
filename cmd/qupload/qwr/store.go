7cb28108e663b1d78074c30843cf5b3294f67f03
Krishnanand Thommandra <kthommandra@arista.com>
2017-04-03 22:07:33 -0700

    Fixed after testing

--- cmd/qupload/qwr/store.go
+++ cmd/qupload/qwr/store.go
@@ -3,25 +3,24 @@
 
 package qwr
 
-import "fmt"
 import "strings"
 
 import "github.com/aristanetworks/quantumfs"
 import "github.com/aristanetworks/quantumfs/hash"
 import "github.com/aristanetworks/quantumfs/testutils"
 
+// create the workspace if it doesn't exist
+// then advance it to the newKey
 func createAdvance(qctx *quantumfs.Ctx, wsdb quantumfs.WorkspaceDB,
 	wsname string,
 	newKey quantumfs.ObjectKey) error {
-	wsParts := strings.Split(wsname, "/")
 
-	fmt.Println("Checking ", wsname)
+	wsParts := strings.Split(wsname, "/")
 	curKey, err := wsdb.Workspace(qctx,
 		wsParts[0], wsParts[1], wsParts[2])
 	if err != nil {
 		if wsdbErrCode, ok := err.(*quantumfs.WorkspaceDbErr); ok {
 			if wsdbErrCode.Code == quantumfs.WSDB_WORKSPACE_NOT_FOUND {
-				fmt.Println(wsname, " doesn't exist, so creating...")
 				err = wsdb.BranchWorkspace(qctx,
 					quantumfs.NullSpaceName,
 					quantumfs.NullSpaceName,
@@ -29,17 +28,13 @@ func createAdvance(qctx *quantumfs.Ctx, wsdb quantumfs.WorkspaceDB,
 					wsParts[0], wsParts[1], wsParts[2])
 			}
 		}
-
 		if err != nil {
 			return err
 		}
-
-		fmt.Println("Checking ", wsname)
 		curKey, err = wsdb.Workspace(qctx,
 			wsParts[0], wsParts[1], wsParts[2])
 	}
 
-	fmt.Println("Advancing ", wsname, curKey, newKey)
 	_, err = wsdb.AdvanceWorkspace(qctx,
 		wsParts[0], wsParts[1], wsParts[2],
 		curKey, newKey)
