2a99acdd96453adfa3da789d8a1a9bcc06bb6ce5
Peter Neufeld <pwn@arista.com>
2017-04-19 11:47:44 -0700

    Added test helper function to simplify tests

--- daemon/Merge_test.go
+++ daemon/Merge_test.go
@@ -5,12 +5,9 @@ package daemon
 
 // Test workspace merging
 
-import "bytes"
-import "io/ioutil"
 import "os"
 import "testing"
 
-import "github.com/aristanetworks/quantumfs/testutils"
 
 func TestBasicMerge(t *testing.T) {
 	runTest(t, func(test *testHelper) {
@@ -27,18 +24,10 @@ func TestBasicMerge(t *testing.T) {
 		err = os.MkdirAll(workspaceB + "/subdir", 0777)
 		test.AssertNoErr(err)
 
-		data := string(GenData(1000))
-		err = testutils.PrintToFile(fileA, data[:100])
-		test.AssertNoErr(err)
-
-		err = testutils.PrintToFile(fileB, data[:300])
-		test.AssertNoErr(err)
-
-		err = testutils.PrintToFile(fileC, data[:500])
-		test.AssertNoErr(err)
-
-		err = testutils.PrintToFile(fileC2, data[400:900])
-		test.AssertNoErr(err)
+		dataA := test.MakeFile(fileA)
+		dataB := test.MakeFile(fileB)
+		test.MakeFile(fileC)
+		dataC2 := test.MakeFile(fileC2)
 
 		test.SyncAllWorkspaces()
 
@@ -46,19 +35,8 @@ func TestBasicMerge(t *testing.T) {
 		err = api.Merge(test.RelPath(workspaceB), test.RelPath(workspaceA))
 		test.AssertNoErr(err)
 
-		readData, err := ioutil.ReadFile(fileA)
-		test.AssertNoErr(err)
-		test.Assert(bytes.Equal([]byte(data[:100]), readData),
-			"pre-existing file corrupted in merge")
-
-		readData, err = ioutil.ReadFile(workspaceA + "/fileB")
-		test.AssertNoErr(err)
-		test.Assert(bytes.Equal([]byte(data[:300]), readData),
-			"merged in file isn't correct")
-
-		readData, err = ioutil.ReadFile(fileC)
-		test.AssertNoErr(err)
-		test.Assert(bytes.Equal([]byte(data[400:900]), readData),
-			"overwritten file during merge doesn't choose newer file")
+		test.CheckData(fileA, dataA)
+		test.CheckData(workspaceA + "/fileB", dataB)
+		test.CheckData(fileC, dataC2)
 	})
 }
