83e180f14cb8027e2d2764388f7f0062488e56ce
Travis Brown <travisb@arista.com>
2017-04-24 15:19:34 -0700

    Test GID parsing

--- daemon/SupplementaryGroups_test.go
+++ daemon/SupplementaryGroups_test.go
@@ -17,5 +17,18 @@ func TestGroupParsing(t *testing.T) {
 		// Sanity test that we fail to match when there is no match
 		test.Assert(!hasMatchingGid(test.newCtx(), 99, pid, 1000),
 			"No matching failed")
+
+		// Match the passed in gid even if it isn't in the groups line
+		test.Assert(hasMatchingGid(test.newCtx(), 99, pid, 99),
+			"Matching failed")
+
+		test.Assert(hasMatchingGid(test.newCtx(), 99, pid, 0),
+			"Matching failed")
+
+		test.Assert(hasMatchingGid(test.newCtx(), 99, pid, 30),
+			"Matching failed")
+
+		test.Assert(hasMatchingGid(test.newCtx(), 99, pid, 200),
+			"Matching failed")
 	})
 }
