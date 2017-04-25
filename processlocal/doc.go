27a4eb24d7720b73a6d560b272975e0350aba2f9
Travis Brown <travisb@arista.com>
2017-04-25 14:35:36 -0700

    Improve processlocal documentation and exports

---
+++ processlocal/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+// In-memory implementations of the QuantumFS backends primarily intended for
+// tests.
+package processlocal
