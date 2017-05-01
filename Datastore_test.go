abd9382e7a04342fb58f30ae4e5e36f5329897af
Yang <yang@arista.com>
2017-05-01 10:24:41 -0700

    add the test for measuring the cache size

--- Datastore_test.go
+++ Datastore_test.go
@@ -7,7 +7,7 @@ import "testing"
 
 func TestDirectoryRecordSort(t *testing.T) {
 	runTest(t, func(test *testHelper) {
-		_, dirEntry := NewDirectoryEntry(MaxDirectoryRecords())
+		_, dirEntry := NewDirectoryEntry(4)
 		dirEntry.SetNumEntries(3)
 
 		dr1 := NewDirectoryRecord()
@@ -32,6 +32,29 @@ func TestDirectoryRecordSort(t *testing.T) {
 			"Wrong sort. Found %s expects \"name2\"",
 			dirEntry.Entry(2).Filename())
 
-		test.Assert(false, "The size of the block %d", BlockSize())
+	})
+}
+
+func TestCacheSize(t *testing.T) {
+	runTest(t, func(test *testHelper) {
+		// Measure the cache block size of directory entries
+		remain, dirEntry := NewDirectoryEntry(4)
+		cacheNum := dirEntry.dir.Entries().Len()
+		test.Assert(remain == 0 && cacheNum == 4, "Incorrect size of the "+
+			"remain: %d != 0 and of the cache: %d != 4",
+			remain, cacheNum)
+
+		// Measure the cache block size of very large files
+		vlf := NewVeryLargeFile(5)
+		cacheNum = vlf.vlf.LargeFileKeys().Len()
+		test.Assert(cacheNum == 5, "Incorrect size of the cache: %d != 5",
+			cacheNum)
+
+		// Measure the cache block size of Extended Attributes
+		remain, hlEntry := NewHardlinkEntry(6)
+		cacheNum = hlEntry.entry.Entries().Len()
+		test.Assert(remain == 0 && cacheNum == 6, "Incorrect size of the "+
+			"remain: %d != 0 and of the cache: %d != 4",
+			remain, cacheNum)
 	})
 }
