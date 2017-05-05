67151e6187ebc51dec192521debb19dc893995cf
Peter Neufeld <pwn@arista.com>
2017-05-05 11:47:26 -0700

    Initially working Qfsclientc test that actually runs quantumfs

--- qfsclientc/qfsclientc.go
+++ qfsclientc/qfsclientc.go
@@ -13,12 +13,19 @@ package qfsclientc
 const char * cGetApi(uint32_t *apiHandleOut);
 const char * cGetApiPath(const char *path, uint32_t *apiHandleOut);
 const char * cReleaseApi(uint32_t apiHandle);
+const char * cGetBlock(uint32_t apiHandle, const char *key, char *dataOut,
+	uint32_t *lenOut);
+const char * cSetBlock(uint32_t apiHandle, const char *key, uint8_t *data,
+	uint32_t len);
 
 */
 import "C"
 
 import "errors"
 import "unsafe"
+
+import "github.com/aristanetworks/quantumfs"
+
 
 type QfsClientApi struct {
 	handle	uint32
@@ -60,3 +67,31 @@ func ReleaseApi(api QfsClientApi) error {
 
 	return nil
 }
+
+func (api * QfsClientApi) SetBlock(key string, data []byte) error {
+	errStr := C.GoString(C.cSetBlock(C.uint32_t(api.handle), C.CString(key),
+		(*C.uint8_t)(unsafe.Pointer(&data)),
+		C.uint32_t(len(data))))
+
+	if errStr != "" {
+		return errors.New(errStr)
+	}
+
+	return nil
+}
+
+func (api * QfsClientApi) GetBlock(key string) ([]byte, error) {
+	data := make([]byte, quantumfs.MaxBlockSize)
+	var dataLen uint32
+
+	errStr := C.GoString(C.cGetBlock(C.uint32_t(api.handle), C.CString(key),
+		(*C.char)(unsafe.Pointer(&data)),
+		(*C.uint32_t)(unsafe.Pointer(&dataLen))))
+
+	if errStr != "" {
+		return nil, errors.New(errStr)
+	}
+
+	return data[:dataLen], nil
+}
+
