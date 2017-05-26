6ad75094b2e485fb5c7410d608b487befbe3c83f
Shayan Pooya <shayan@arista.com>
2017-05-26 16:26:50 -0700

    fix the condition for rejecting a new xattr

--- daemon/directory.go
+++ daemon/directory.go
@@ -1548,7 +1548,7 @@ func (dir *Directory) setChildXAttr(c *ctx, inodeNum InodeId, attr string,
 
 	// Append attribute
 	if !set {
-		if attributeList.NumAttributes() >
+		if attributeList.NumAttributes() >=
 			quantumfs.MaxNumExtendedAttributes() {
 
 			c.vlog("XAttr list full %d", attributeList.NumAttributes())
