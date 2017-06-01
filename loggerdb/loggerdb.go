c7e4adf70be8884a67e88daf85460b9167065f2e
Peter Neufeld <pwn@arista.com>
2017-06-01 14:30:54 -0700

    Working implementation with printf output

--- loggerdb/loggerdb.go
+++ loggerdb/loggerdb.go
@@ -3,9 +3,12 @@
 
 package qloggerdb
 
+import "container/list"
+
 import "github.com/aristanetworks/quantumfs/qlog"
 
-const requestEndAfterNs = 1000000000
+
+const requestEndAfterNs = 3000000000
 
 type DbInterface interface {
 	Store(tag string, timeNs uint64)
@@ -13,23 +16,86 @@ type DbInterface interface {
 	Fetch(tag string, lastN int) []uint64
 }
 
+type logTrack struct {
+	logs		qlog.LogStack
+	listElement	*list.Element
+}
+
+type trackerKey struct {
+	reqId		uint64
+	lastLogTime	int64
+}
+
+type statExtractor interface {
+	ProcessRequest(request qlog.LogStack)
+}
+
 type LoggerDb struct {
-	logsByRequest	map[uint64]qlog.LogStack
+	logsByRequest	map[uint64]logTrack
+
+	// track the oldest untouched requests so we can push them to the stat
+	// extractors after the resting period (so we're confident there are no
+	// more logs coming for each request)
+	requestSequence	list.List
+
+	statExtractors	[]statExtractor
 }
 
 func NewLoggerDb(db DbInterface) *LoggerDb {
-	return &LoggerDb {
-		logsByRequest:	make(map[uint64]qlog.LogStack, 0),
+	rtn := LoggerDb {
+		logsByRequest:	make(map[uint64]logTrack),
+		statExtractors:	make([]statExtractor, 0),
 	}
+
+	// sample extractor
+	rtn.statExtractors = append(rtn.statExtractors, newExtPairAvg(db,
+		"---In Mux::GetAttr", "Out-- Mux::GetAttr"))
+
+	return &rtn
 }
 
 func (logger *LoggerDb) ProcessLog(v qlog.LogOutput) {
-	var logs qlog.LogStack
-	if series, exists := logger.logsByRequest[v.ReqId]; exists {
-		logs = series
+	var tracker logTrack
+	if tracker_, exists := logger.logsByRequest[v.ReqId]; exists {
+		tracker = tracker_
+		logger.requestSequence.MoveToBack(tracker.listElement)
 	} else {
-		logs = make([]qlog.LogOutput, 0)
+		tracker.logs = make([]qlog.LogOutput, 0)
+		newElem := logger.requestSequence.PushBack(trackerKey {
+			reqId:		v.ReqId,
+			lastLogTime:	v.T,
+		})
+		tracker.listElement = newElem
+	}
+	tracker.logs = append(tracker.logs, v)
+	logger.logsByRequest[v.ReqId] = tracker
+
+	// Update the record of the last time we saw a log for this request
+	trackerElem := tracker.listElement.Value.(trackerKey)
+	if v.T > trackerElem.lastLogTime {
+		trackerElem.lastLogTime = v.T
 	}
 
-	logs = append(logs, v)
+	// now check if any requests are old and ready to go to the extractors
+	for {
+		requestElem := logger.requestSequence.Front()
+
+		if requestElem == nil {
+			break
+		}
+
+		request := requestElem.Value.(trackerKey)
+		if v.T - request.lastLogTime > requestEndAfterNs {
+			logger.requestSequence.Remove(requestElem)
+			reqLogs := logger.logsByRequest[request.reqId]
+			delete(logger.logsByRequest, request.reqId)
+
+			for _, e := range logger.statExtractors {
+				e.ProcessRequest(reqLogs.logs)
+			}
+		} else {
+			// No more requests ready to extract stats
+			break
+		}
+	}
 }
