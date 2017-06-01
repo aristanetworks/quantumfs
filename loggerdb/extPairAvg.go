2fb1ba644dddd1ea710f106fb746f63f2569c8ac
Peter Neufeld <pwn@arista.com>
2017-06-01 14:30:54 -0700

    Working implementation with printf output

---
+++ loggerdb/extPairAvg.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+// extPairAvg is a stat extractor that extracts the average duration between two
+// log format strings. This would most often be used to time between a fnIn and fnOut
+
+package qloggerdb
+
+import "fmt"
+
+import "github.com/aristanetworks/quantumfs/qlog"
+
+
+type extPairAvg struct {
+	db		DbInterface
+	fmtStart	string
+	fmtStop		string
+}
+
+func newExtPairAvg(db DbInterface, start string, stop string) *extPairAvg {
+	return &extPairAvg {
+		fmtStart:	start,
+		fmtStop:	stop,
+	}
+}
+
+func (ext *extPairAvg) ProcessRequest(request qlog.LogStack) {
+	for _, v := range request {
+		fmt.Printf("%s", v.ToString())
+	}
+
+	// TODO: Extract the stat and output to memdb
+	// ext.db.Store(ext.fmtStart, newStatistic)
+}
