1766067327acb7d166f5d0adc563d795467db38c
Shayan Pooya <shayan@arista.com>
2017-06-06 16:48:32 -0700

    gut check fixup

--- daemon/directory.go
+++ daemon/directory.go
@@ -426,7 +426,8 @@ func (dir *Directory) setChildAttr(c *ctx, inodeNum InodeId,
 	defer c.funcIn("Directory::setChildAttr").Out()
 
 	if dir.isOrphaned() && dir.id == inodeNum {
-		return dir.setOrphanChildAttr(c, inodeNum, newType, attr, out, updateMtime)
+		return dir.setOrphanChildAttr(c, inodeNum, newType, attr, out,
+			updateMtime)
 	}
 
 	result := func() fuse.Status {
