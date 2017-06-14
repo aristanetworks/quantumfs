46e83c7e3833dcb895430caa51b20515f087b383
Peter Neufeld <pwn@arista.com>
2017-06-14 11:54:02 -0700

    context switch

--- qlogstats/Logger_test.go
+++ qlogstats/Logger_test.go
@@ -5,43 +5,41 @@ package qlogstats
 
 import (
 	"testing"
+	"time"
 
-	"github.com/aristanetworks/quantumfs/daemon"
-	"github.com/aristanetworks/quantumfs/testutils"
+	"github.com/aristanetworks/quantumfs/qlog"
+	"github.com/aristanetworks/quantumfs/processlocal"
 )
 
-func runReader(qlogFile string, extractors []qlogstats.StatExtractorConfig,
-	fxn func(qlog.LogOutput)) processlocal.Memdb {
-
-	reader := qlog.NewReader(qlogFile)
+func runReader(qlogFile string,
+	extractors []StatExtractorConfig) *processlocal.Memdb {
 
 	db := processlocal.NewMemdb()
-
-	logger := qlogstats.NewLoggerDb(db, extractors)
-	reader.ProcessLogs(qlog.ReadOnly, func(v qlog.LogOutput) {
-		logger.ProcessLog(v)
-	})
+	AggregateLogs(qlogFile, db, extractors)
 
 	return db
 }
 
 func TestMatches(t *testing.T) {
 	runTest(t, func(test *testHelper) {
-		qlogHandle := test.qfs.c.Ctx.Qlog
+		qlogHandle := test.Logger
 
 		// Artificially insert matching logs
-		qlogHandle.Log(LogTest, 12345, 2, qlog.FnEnterStr+"TestMatch")
-		qlogHandle.Log(LogTest, 12346, 3, qlog.FnExitStr+"TestMatch")
+		qlogHandle.Log(qlog.LogTest, 12345, 2, qlog.FnEnterStr+"TestMatch")
+		qlogHandle.Log(qlog.LogTest, 12346, 3, qlog.FnExitStr+"TestMatch")
 
 		// Setup an extractor
-		extractors := make([]qlogstats.StatExtractorConfig, 0)
-		extractors = append(extractors, qlogstats.NewStatExtractorConfig(
-			qlogstats.NewExtPairStats(qlog.FnEnterStr+"TestMatch",
-				qlog.FnExitStr+"TestMatch", true),
+		extractors := make([]StatExtractorConfig, 0)
+		extractors = append(extractors, NewStatExtractorConfig(
+			NewExtPairStats(qlog.FnEnterStr+"TestMatch",
+				qlog.FnExitStr+"TestMatch", true, "TestMatch"),
 			(5*time.Second)))
 
 		// Run the reader
-		memdb, reader := runReader(test.CachePath+"/ramfs/qlog", extractors)
+		memdb := runReader(test.CachePath+"/ramfs/qlog", extractors)
 
+		test.WaitFor("statistic to register", func () bool {
+
+		})
 	})
 }
