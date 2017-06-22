e3a0bfad5cd0b084b379b1f39239125f3892df38
Peter Neufeld <pwn@arista.com>
2017-06-22 13:34:28 -0700

    Review changes

--- thirdparty_backends/influxdb.go
+++ thirdparty_backends/influxdb.go
@@ -1,11 +1,11 @@
 // +build !skip_backends
 
-// Copyright (c) 2016 Arista Networks, Inc.  All rights reserved.
+// Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
 // Arista Networks, Inc. Confidential and Proprietary.
 
 package thirdparty_backends
 
-// To avoid compiling in support for ether datastore
+// To avoid (compiling in) support for influxDB
 // change "!skip_backends" in first line with "ignore"
 
 import (
