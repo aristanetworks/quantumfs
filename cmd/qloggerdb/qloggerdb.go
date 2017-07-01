d084fea0c0ebe00aecfd2890470b36296f147a6b
Peter Neufeld <pwn@arista.com>
2017-06-30 18:30:59 -0700

    Initial set of extractors

--- cmd/qloggerdb/qloggerdb.go
+++ cmd/qloggerdb/qloggerdb.go
@@ -43,6 +43,74 @@ func loadTimeSeriesDB() quantumfs.TimeSeriesDB {
 	return tsdb
 }
 
+const statPeriod = 5 * time.Second
+
+func newQfsExtPair(common string,
+	startPostfix string) qlogstats.StatExtractorConfig {
+
+	return qlogstats.NewStatExtractorConfig(
+		qlogstats.NewExtPairStats(
+			qlog.FnEnterStr+common+" "+startPostfix+"\n",
+			qlog.FnExitStr+common+"\n",
+			true, common),
+		statPeriod)
+}
+
+func createExtractors() []qlogstats.StatExtractorConfig {
+	return []qlogstats.StatExtractorConfig {
+		qlogstats.NewStatExtractorConfig(
+			qlogstats.NewExtPointStats("Found key in readcache",
+			"cache_hit"), statPeriod),
+		qlogstats.NewStatExtractorConfig(
+			qlogstats.NewExtPointStats("Cache miss",
+			"cache_miss"), statPeriod),
+
+		newQfsExtPair("EtherBlobStoreTranslator::Get", "key %s"),
+		newQfsExtPair("EtherBlobStoreTranslator::Set", "key %s"),
+		newQfsExtPair("EtherWsdbTranslator::TypespaceList", ""),
+		newQfsExtPair("EtherWsdbTranslator::NamespaceList", "typespace: %s"),
+		newQfsExtPair("EtherWsdbTranslator::WorkspaceList", "%s/%s"),
+		newQfsExtPair("EtherWsdbTranslator::Workspace", "%s/%s/%s"),
+		newQfsExtPair("EtherWsdbTranslator::BranchWorkspace",
+			"%s/%s/%s -> %s/%s/%s"),
+		newQfsExtPair("EtherWsdbTranslator::AdvanceWorkspace",
+			"%s/%s/%s %s -> %s"),
+		newQfsExtPair("Mux::syncAll", ""),
+		newQfsExtPair("Mux::Lookup", "Inode %d Name %s"),
+		newQfsExtPair("Mux::Forget", ""),
+		newQfsExtPair("Mux::GetAttr", "Inode %d"),
+		newQfsExtPair("Mux::SetAttr", "Inode %d"),
+		newQfsExtPair("Mux::Mknod", "Inode %d Name %s"),
+		newQfsExtPair("Mux::Mkdir", "Inode %d Name %s"),
+		newQfsExtPair("Mux::Unlink", "Inode %d Name %s"),
+		newQfsExtPair("Mux::Rmdir", "Inode %d Name %s"),
+		newQfsExtPair("Mux::Rename", "Inode %d newdir %d %s -> %s"),
+		newQfsExtPair("Mux::Link", "inode %d to name %s in dstDir %d"),
+		newQfsExtPair("Mux::Symlink", "Inode %d Name %s"),
+		newQfsExtPair("Mux::Readlink", "Inode %d"),
+		newQfsExtPair("Mux::Access", "Inode %d"),
+		newQfsExtPair("Mux::GetXAttrSize", "Inode %d"),
+		newQfsExtPair("Mux::GetXAttrData", "Inode %d"),
+		newQfsExtPair("Mux::ListXAttr", "Inode %d"),
+		newQfsExtPair("Mux::SetXAttr", "Inode %d"),
+		newQfsExtPair("Mux::RemoveXAttr", "Inode %d"),
+		newQfsExtPair("Mux::Create", "Inode %d Name %s"),
+		newQfsExtPair("Mux::Open", "Inode %d"),
+		newQfsExtPair("Mux::Read", "Fh: %d"),
+		newQfsExtPair("Mux::Release", "Fh: %v"),
+		newQfsExtPair("Mux::Write", "Fh: %d"),
+		newQfsExtPair("Mux::Flush", "Fh: %v Context %d %d %d"),
+		newQfsExtPair("Mux::Fsync", "Enter Fh %d"),
+		newQfsExtPair("Mux::Fallocate", ""),
+		newQfsExtPair("Mux::OpenDir", "Inode %d"),
+		newQfsExtPair("Mux::ReadDir", "Fh: %d offset %d"),
+		newQfsExtPair("Mux::ReadDirPlus", "Fh: %d offset %d"),
+		newQfsExtPair("Mux::ReleaseDir", "Fh: %d"),
+		newQfsExtPair("Mux::FsyncDir", "Fh: %d"),
+		newQfsExtPair("Mux::StatFs", ""),
+	}
+}
+
 func main() {
 	flag.Parse()
 	if len(os.Args) < 2 {
