7dad2989b5d65061f30fadcd623ef0e7cd34c3ef
Travis Brown <travisb@arista.com>
2017-08-03 18:01:23 -0700

    Add a basic test of subscriptions

--- grpc/server/BasicApi_test.go
+++ grpc/server/BasicApi_test.go
@@ -7,6 +7,8 @@ package server
 
 import (
 	"testing"
+
+	"github.com/aristanetworks/quantumfs"
 )
 
 func TestConnect(t *testing.T) {
@@ -16,5 +18,28 @@ func TestConnect(t *testing.T) {
 		num, err := client.NumTypespaces(test.ctx)
 		test.AssertNoErr(err)
 		test.Assert(num == 1, "Unexpected number of typespaces %d", num)
+	})
+}
+
+func TestSubscribeAndNotify(t *testing.T) {
+	runTest(t, func(test *testHelper) {
+		client := test.newClient()
+
+		called := false
+		callback := func(updates map[string]quantumfs.WorkspaceState) {
+			test.Assert(len(updates) == 1, "Wrong number of updates: %d",
+				len(updates))
+			called = true
+		}
+
+		client.SetCallback(callback)
+
+		test.AssertNoErr(client.SubscribeTo("test/test/test"))
+
+		test.AssertNoErr(client.BranchWorkspace(test.ctx,
+			quantumfs.NullSpaceName, quantumfs.NullSpaceName,
+			quantumfs.NullSpaceName, "test", "test", "test"))
+
+		test.WaitFor("Callback to be invoked", func() bool { return called })
 	})
 }
