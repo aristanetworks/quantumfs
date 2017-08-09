44ebe26d10064272acf3f9bec1ef5795be505ab4
Travis Brown <travisb@arista.com>
2017-08-09 12:15:11 -0700

    Add test of subscriptions across server restart

--- grpc/server/ClientTests.go
+++ grpc/server/ClientTests.go
@@ -26,3 +26,41 @@ func TestServerDisconnection(t *testing.T) {
 		test.Assert(num == 1, "Incorrect number of typespaces: %d", num)
 	})
 }
+
+func TestSubscriptionsAcrossDisconnection(t *testing.T) {
+	runTest(t, func(test *testHelper) {
+		client := test.newClient()
+
+		test.AssertNoErr(client.SubscribeTo("test1/test/test"))
+		test.AssertNoErr(client.SubscribeTo("test2/test/test"))
+		test.AssertNoErr(client.SubscribeTo("test3/test/test"))
+
+		updated := map[string]bool{}
+		callback := func(updates map[string]quantumfs.WorkspaceState) {
+			for workspace, _ := range updates {
+				updated[workspace] = true
+			}
+		}
+		client.SetCallback(callback)
+
+		test.restartServer()
+
+		err := client.BranchWorkspace(test.ctx, quantumfs.NullSpaceName,
+			quantumfs.NullSpaceName, quantumfs.NullSpaceName,
+			"test", "test", "test")
+		test.AssertNoErr(err)
+
+		err = client.BranchWorkspace(test.ctx, quantumfs.NullSpaceName,
+			quantumfs.NullSpaceName, quantumfs.NullSpaceName,
+			"test2", "test", "test")
+		test.AssertNoErr(err)
+
+		test.WaitFor("to receive workspace notification", func() bool {
+			_, exists := updated["test2/test/test"]
+			return exists
+		})
+
+		_, exists := updated["test/test/test"]
+		test.Assert(!exists, "Invalid workspace notification received")
+	})
+}
