71ee417378d339dfa7abfd9d24a07a77082e0984
Shayan Pooya <shayan@arista.com>
2017-08-28 12:25:29 -0700

    Protect against concurrent inode instantiation
    The instantiationLock is an rw lock which allows multiple parties
    instantiating a single inode which results in inconsistent state and a
    random crash somewhere in the near future.
    Use per-inode locks to prevent such instantiations.

--- daemon/workspaceroot.go
+++ daemon/workspaceroot.go
@@ -249,6 +249,11 @@ func (wsr *WorkspaceRoot) instantiateChild(c *ctx, inodeNum InodeId) (Inode,
 		return wsr.hardlinks[id].record
 	}()
 	if hardlinkRecord != nil {
+		defer wsr.linkLock.Lock().Unlock()
+		if inode := c.qfs.inodeNoInstantiate(c, inodeNum); inode != nil {
+			c.vlog("Someone has already instantiated inode %d", inodeNum)
+			return inode, nil
+		}
 		return wsr.Directory.recordToChild(c, inodeNum, hardlinkRecord)
 	}
 
