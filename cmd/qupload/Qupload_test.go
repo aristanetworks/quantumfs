8bbbe46593d82bd5bbbbfdc138e4317383c5d2e1
Peter Neufeld <pwn@arista.com>
2017-09-01 12:23:45 -0700

    Transplant of quploadCheck test framework

---
+++ cmd/qupload/Qupload_test.go
@@ -0,0 +1,93 @@
+// Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+package main
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/aristanetworks/quantumfs"
+	"github.com/aristanetworks/quantumfs/testutils"
+	"golang.org/x/net/context"
+	"golang.org/x/sync/errgroup"
+)
+
+func (th *testHelper) checkUploadMatches(checkPath string, workspace string,
+	compare func(quantumfs.DirectoryRecord, quantumfs.DirectoryRecord)) {
+
+	c := Ctx{
+		Qctx: &(th.TestCtx().Ctx),
+	}
+	_, c.eCtx = errgroup.WithContext(context.Background())
+
+	fromWalker := make(chan *pathInfo, 100)
+
+	err := filepath.Walk(checkPath,
+		func(path string, info os.FileInfo, err error) error {
+			return pathWalker(&c, fromWalker, path, checkPath, info, err)
+		})
+	th.AssertNoErr(err)
+
+	checkedRoot := false
+	eof := false
+	for {
+		select {
+		case msg := <-fromWalker:
+			// we only care about the one file we're checking
+			if msg.path != checkPath {
+				continue
+			}
+
+			record, err := processPath(&c, msg)
+			th.AssertNoErr(err)
+
+			checkedRoot = true
+
+			qfsRecord := th.GetRecord(msg.path)
+
+			compare(qfsRecord, record)
+		default:
+			eof = true
+		}
+
+		if eof {
+			break
+		}
+	}
+
+	th.Assert(checkedRoot, "Didn't check %s", checkPath)
+}
+
+func TestFileMatches(t *testing.T) {
+	runTest(t, func(test *testHelper) {
+		workspace := test.NewWorkspace()
+		filename := "testfile123"
+
+		test.AssertNoErr(testutils.PrintToFile(workspace+"/"+filename,
+			"some data"))
+
+		test.checkUploadMatches(workspace+"/"+filename, workspace,
+			func(recA quantumfs.DirectoryRecord,
+				recB quantumfs.DirectoryRecord) {
+
+				recADirect := recA.(*quantumfs.DirectRecord)
+				recBDirect := recB.(*quantumfs.DirectRecord)
+
+				// Everything should match, except fileId and nlinks
+				recBDirect.SetFileId(recADirect.FileId())
+				recBDirect.SetNlinks(recADirect.Nlinks())
+
+				recAData, err := recADirect.MarshalJSON()
+				test.AssertNoErr(err)
+				recBData, err := recBDirect.MarshalJSON()
+				test.AssertNoErr(err)
+
+				test.Assert(bytes.Equal(recAData, recBData),
+					"Record mismatch after qupload: %s\n%s\n",
+					recAData, recBData)
+			})
+	})
+}
