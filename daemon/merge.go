af873375050c75163aa7f928bc491e985214eb7f
Peter Neufeld <pwn@arista.com>
2017-09-22 13:43:13 -0700

    Formatting changes

--- daemon/merge.go
+++ daemon/merge.go
@@ -255,10 +255,10 @@ func mergeDirectory(c *ctx, base quantumfs.ObjectKey,
 			if v.Type() == quantumfs.ObjectTypeDirectory {
 				// Add new links
 				err = traverseSubtree(c, v.ID(),
-					func (v quantumfs.DirectoryRecord) {
+					func(v quantumfs.DirectoryRecord) {
 
-					ht.checkLinkChanged(c, nil, v)
-				})
+						ht.checkLinkChanged(c, nil, v)
+					})
 				if err != nil {
 					return local, err
 				}
@@ -424,20 +424,20 @@ func mergeRecord(c *ctx, base quantumfs.DirectoryRecord,
 
 			// Add new links
 			err = traverseSubtree(c, remote.ID(),
-				func (v quantumfs.DirectoryRecord) {
+				func(v quantumfs.DirectoryRecord) {
 
-				ht.checkLinkChanged(c, nil, v)
-			})
+					ht.checkLinkChanged(c, nil, v)
+				})
 			if err != nil {
 				return nil, err
 			}
 
 			// Remove old links
 			err = traverseSubtree(c, local.ID(),
-				func (v quantumfs.DirectoryRecord) {
+				func(v quantumfs.DirectoryRecord) {
 
-				ht.checkLinkChanged(c, v, nil)
-			})
+					ht.checkLinkChanged(c, v, nil)
+				})
 			if err != nil {
 				return nil, err
 			}
