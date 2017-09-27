c9e6057b0cdb67ad87b3a0e38a2aa399f14e512b
Peter Neufeld <pwn@arista.com>
2017-09-27 15:05:19 -0700

    Review changes

--- daemon/merge.go
+++ daemon/merge.go
@@ -523,7 +523,7 @@ func mergeFile(c *ctx, base quantumfs.DirectoryRecord,
 	if iterator != nil && other != nil &&
 		local.FileId() == remote.FileId() {
 
-		// Perform an intra-file merge by iterating through the small file,
+		// Perform an intra-file merge by iterating through the shorter file,
 		// writing its changes to other, and then keeping other
 		otherIsOlder := (iteratorRecord.ModificationTime() >
 			otherRecord.ModificationTime())
@@ -536,7 +536,6 @@ func mergeFile(c *ctx, base quantumfs.DirectoryRecord,
 		// reconciling the accessor type - the size won't change this way
 		operateOnBlocks(c, iterator, 0, uint32(other.fileLength(c)),
 			func(c *ctx, blockIdx int, offset uint64) error {
-
 				var err error
 				baseRead := 0
 				if base != nil {
