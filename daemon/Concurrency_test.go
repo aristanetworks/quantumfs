e5eb5bdc1f72c39772d7008a7cf1bb927d6782c4
Peter Neufeld <pwn@arista.com>
2017-10-03 11:11:19 -0700

    Added framework of first test

--- daemon/Concurrency_test.go
+++ daemon/Concurrency_test.go
@@ -5,3 +5,35 @@ package daemon
 
 // Test concurrent workspaces
 
+import (
+	"bytes"
+	"testing"
+	"io/ioutil"
+
+	"github.com/aristanetworks/quantumfs"
+	"github.com/aristanetworks/quantumfs/testutils"
+)
+
+func TestConcurrentReadWrite(t *testing.T) {
+	runDualQuantumFsTest(t, func(test *testHelper) {
+		workspace0 := test.NewWorkspace()
+		mnt1 := test.qfsInstances[1].config.MountPath
+		workspaceName := test.RelPath(workspace0)
+		workspace1 := mnt1 + "/" +  workspaceName
+
+		api1, err := quantumfs.NewApiWithPath(mnt1 + "/api")
+		test.AssertNoErr(err)
+		defer api1.Close()
+		test.AssertNoErr(api1.EnableRootWrite(workspaceName))
+
+		data := []byte("abc")
+		testFile := "/file"
+		test.AssertNoErr(testutils.PrintToFile(workspace0 + testFile,
+			string(data)))
+
+		readData, err := ioutil.ReadFile(workspace1 + testFile)
+		test.AssertNoErr(err)
+		test.Assert(bytes.Equal(readData, data),
+			"concurrent read/write broken")
+	})
+}
