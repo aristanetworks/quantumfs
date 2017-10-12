f887e7e48189a16167a208d5a5de135594ca16d8
Shayan Pooya <shayan@arista.com>
2017-10-12 09:19:30 -0700

    Improve the RenameOnto tests to catch a race with the flusher goroutines

--- daemon/Directory_test.go
+++ daemon/Directory_test.go
@@ -1039,7 +1039,9 @@ func TestDirectoryMvChildOntoOpenFileSameDir(t *testing.T) {
 		f, err := os.Open(file2)
 		test.AssertNoErr(err)
 		defer f.Close()
+		syncErr := test.SyncWorkspaceAsync(test.RelPath(workspace))
 		test.AssertNoErr(syscall.Rename(file1, file2))
+		test.AssertNoErr(<-syncErr)
 	})
 }
 
@@ -1057,7 +1059,9 @@ func TestDirectoryMvChildOntoOpenFileInParentDir(t *testing.T) {
 		f, err := os.Open(file2)
 		test.AssertNoErr(err)
 		defer f.Close()
+		syncErr := test.SyncWorkspaceAsync(test.RelPath(workspace))
 		test.AssertNoErr(syscall.Rename(file1, file2))
+		test.AssertNoErr(<-syncErr)
 	})
 }
 
@@ -1077,7 +1081,9 @@ func TestDirectoryMvChildOntoOpenFileInSibling(t *testing.T) {
 		f, err := os.Open(file2)
 		test.AssertNoErr(err)
 		defer f.Close()
+		syncErr := test.SyncWorkspaceAsync(test.RelPath(workspace))
 		test.AssertNoErr(syscall.Rename(file1, file2))
+		test.AssertNoErr(<-syncErr)
 	})
 }
 
