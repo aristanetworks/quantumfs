ed20f0e40ce1b905d49ea567d91e9c69783d97b5
Travis Brown <travisb@arista.com>
2017-10-16 10:35:03 -0700

    Organize log messages by type

--- cmd/qloggerdb/qloggerdb.go
+++ cmd/qloggerdb/qloggerdb.go
@@ -57,7 +57,10 @@ func newQfsExtPair(common string,
 
 func createExtractors() []qlogstats.StatExtractor {
 	return []qlogstats.StatExtractor{
+		// Critical system errors
 		qlogstats.NewExtPointStatsPartialFormat("ERROR: ", "SystemErrors"),
+
+		// Cache hits and misses
 		qlogstats.NewExtPointStats(daemon.CacheHitLog, "readcache_hit"),
 		qlogstats.NewExtPointStats(daemon.CacheMissLog, "readcache_miss"),
 
@@ -68,10 +71,13 @@ func createExtractors() []qlogstats.StatExtractor {
 		qlogstats.NewExtPointStats(thirdparty_backends.EtherTtlCacheEvict,
 			"ether_setcache_evict"),
 
+		// Data store latency
 		newQfsExtPair(thirdparty_backends.EtherGetLog,
 			thirdparty_backends.KeyLog),
 		newQfsExtPair(thirdparty_backends.EtherSetLog,
 			thirdparty_backends.KeyLog),
+
+		// Workspace DB latency
 		newQfsExtPair(thirdparty_backends.EtherTypespaceLog, ""),
 		newQfsExtPair(thirdparty_backends.EtherNamespaceLog,
 			thirdparty_backends.EtherNamespaceDebugLog),
@@ -83,8 +89,8 @@ func createExtractors() []qlogstats.StatExtractor {
 			thirdparty_backends.EtherBranchDebugLog),
 		newQfsExtPair(thirdparty_backends.EtherAdvanceLog,
 			thirdparty_backends.EtherAdvanceDebugLog),
-		newQfsExtPair(daemon.SyncAllLog, ""),
-		newQfsExtPair(daemon.SyncWorkspaceLog, ""),
+
+		// FUSE Requests
 		newQfsExtPair(daemon.LookupLog, daemon.InodeNameLog),
 		newQfsExtPair(daemon.ForgetLog, ""),
 		newQfsExtPair(daemon.GetAttrLog, daemon.InodeOnlyLog),
@@ -115,9 +121,15 @@ func createExtractors() []qlogstats.StatExtractor {
 		newQfsExtPair(daemon.ReadDirLog, daemon.FileOffsetLog),
 		newQfsExtPair(daemon.ReadDirPlusLog, daemon.FileOffsetLog),
 		newQfsExtPair(daemon.ReleaseDirLog, daemon.FileHandleLog),
-		newQfsExtPair(daemon.ReleaseFileHandleLog, ""),
 		newQfsExtPair(daemon.FsyncDirLog, daemon.FileHandleLog),
 		newQfsExtPair(daemon.StatFsLog, ""),
+
+		// Top-level QuantumFS filesystem operations
+		newQfsExtPair(daemon.ReleaseFileHandleLog, ""),
+		newQfsExtPair(daemon.SyncWorkspaceLog, ""),
+		newQfsExtPair(daemon.SyncAllLog, ""),
+
+		// Ether.CQL internal statistics
 		newQfsExtPair(cql.DeleteLog, cql.KeyLog),
 		newQfsExtPair(cql.GetLog, cql.KeyLog),
 		newQfsExtPair(cql.InsertLog, cql.KeyTTLLog),
