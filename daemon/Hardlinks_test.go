587922859c258ec9933b59a15f0dd2a227972042
Travis Brown <travisb@arista.com>
2017-10-19 17:42:55 -0700

    Test for BUG224729

--- daemon/Hardlinks_test.go
+++ daemon/Hardlinks_test.go
@@ -833,3 +833,35 @@ func TestHardlinkRenameCreation(t *testing.T) {
 			"Mvchild of hardlink doesn't reset creationTime")
 	})
 }
+
+func TestRemoveHardlinkBeforeSync(t *testing.T) {
+	runTest(t, func(test *testHelper) {
+		workspace := test.NewWorkspace()
+
+		dirName := workspace + "/dir"
+		test.AssertNoErr(utils.MkdirAll(dirName, 0777))
+
+		wsr, cleanup := test.GetWorkspaceRoot(workspace)
+		defer cleanup()
+
+		test.Assert(len(wsr.hardlinks) == 0,
+			"Hardlink table not intially empty")
+
+		// Create and remove the hardlink. Though the file is gone the
+		// hardink entry must remain until after all the directories in which
+		// it was a child have flushed, otherwise a directory metadata may
+		// point to an entry which does not exist in the hardlink table.
+		test.createFile(workspace, "dir/leg1", 1)
+		test.linkFile(workspace, "dir/leg1", "leg2")
+		test.removeFile(workspace, "leg2")
+		test.removeFile(workspace, "dir/leg1")
+
+		test.Assert(len(wsr.hardlinks) == 1,
+			"Hardlink table doesn't contain entry after delete")
+
+		test.SyncWorkspace(test.RelPath(workspace))
+
+		test.Assert(len(wsr.hardlinks) == 0,
+			"Hardlink table not empty after sync")
+	})
+}
