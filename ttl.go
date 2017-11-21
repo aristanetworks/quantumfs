3f21dd29078e1e9e5ade4cb40b07ed8b7809ce8e
Sid Agrawal <sid@arista.com>
2017-11-21 11:37:08 -0800

    Add qubit-ttl-tracker daemon
    This daemon regularly walks the entire keyspace and uploads the TTL info
    to influxdb
    
    1/ Review comments from kthommandra
    
    Change-Id: Ia0baf77fd362428f802bf321f2ea3daac6b6fb40
    Testing: Manual testing

--- ttl.go
+++ ttl.go
@@ -60,3 +60,24 @@ func RefreshTTL(c *walker.Ctx, path string, key quantumfs.ObjectKey,
 	}
 	return nil
 }
+
+//GetTTLForKey return the the TTL of the block
+func GetTTLForKey(c *walker.Ctx, cqlds blobstore.BlobStore,
+	key quantumfs.ObjectKey, path string) (int64, error) {
+
+	metadata, err := cqlds.Metadata(ToECtx(c), key.Value())
+	if err != nil {
+		return 0, fmt.Errorf("Metadata failed on path:%v key %v: %v", path, key.String(), err)
+	}
+	ttl, ok := metadata[cql.TimeToLive]
+	if !ok {
+		return 0, fmt.Errorf("Store must return metadata with " +
+			"TimeToLive")
+	}
+	ttlVal, err := strconv.ParseInt(ttl, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("Invalid TTL value in metadata %s ",
+			ttl)
+	}
+	return ttlVal, nil
+}
