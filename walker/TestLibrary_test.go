a3ef00a135002af64c2ca587b8af2f6da250a787
Peter Neufeld <pwn@arista.com>
2017-11-22 21:10:29 -0800

    Test fixes

--- walker/TestLibrary_test.go
+++ walker/TestLibrary_test.go
@@ -158,7 +158,7 @@ func (th *testHelper) readWalkCompare(workspace string, skipDirTest bool) {
 		}
 
 		if skipDirTest && info.IsDir() && strings.HasSuffix(path, "/dir1") {
-			return filepath.SkipError
+			return filepath.SkipDir
 		}
 
 		if path == workspace+"/api" || info.IsDir() {
@@ -213,7 +213,7 @@ func (th *testHelper) readWalkCompare(workspace string, skipDirTest bool) {
 		// NOTE: In the TTL walker this path comparison will be
 		// replaced by a TTL comparison.
 		if skipDirTest && isDir && strings.HasSuffix(path, "/dir1") {
-			return SkipKey
+			return SkipError
 		}
 
 		// Skip, since constant and embedded keys will not
