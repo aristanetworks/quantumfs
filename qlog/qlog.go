8c155fb0d03419847cdacbd0c4cf9ea0fd58d85b
Shayan Pooya <shayan@arista.com>
2018-01-19 15:56:58 -0800

    Copy the arguments of qlog.Log_ in the slow path to reduce constraints in the fast path

--- qlog/qlog.go
+++ qlog/qlog.go
@@ -348,7 +348,11 @@ func (q *Qlog) Log_(t time.Time, idx LogSubsystem, reqId uint64, level uint8,
 	}
 
 	if q.getLogLevel(idx, level) {
-		q.Write(formatString(idx, reqId, t, format), args...)
+		argsCopy := make([]interface{}, 0, len(args))
+		for _, arg := range args {
+			argsCopy = append(argsCopy, arg)
+		}
+		q.Write(formatString(idx, reqId, t, format), argsCopy...)
 	}
 }
 
