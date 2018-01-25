df3c5884b64fe5d47376fb410a053328ceafdf14
Travis Brown <travisb@arista.com>
2018-01-24 17:06:49 -0800

    First cut and combining ChildContainer and ChildMap into one class

--- daemon/childcontainer.go
+++ daemon/childcontainer.go
@@ -14,125 +14,158 @@ import (
 // The combination of the effective and published views gives us a coherent
 // filesystem state from a local user's perspective
 type ChildContainer struct {
-	children map[string]InodeId
 	dir      *Directory
+	children map[string]InodeId
 
 	// Publishable contains a view of the children that is always consistent
 	// The records can be updated in publishable once we are sure they are
 	// pointing to consistent children already present in the datastore
-	publishable ChildMap
+	publishable map[InodeId]map[string]quantumfs.DirectoryRecord
 
 	// Effective contains a partial view of the children that cannot be
 	// published yet. Once the children publish to the datastore, it would be
 	// safe to move their corresponding record from effective to publishable
 	// Effective cannot contain any hardlink records, therefore its
 	// implementation is a simple hashtable from inodes to records
-	effective EffectiveMap
+	effective map[InodeId]map[string]quantumfs.DirectoryRecord
 }
 
-type EffectiveMap struct {
-	m map[InodeId]quantumfs.DirectoryRecord
-}
-
-func newEffectiveMap() *EffectiveMap {
-	return &EffectiveMap{
-		m: make(map[InodeId]quantumfs.DirectoryRecord),
-	}
-}
-
-func (em *EffectiveMap) get(inodeId InodeId) quantumfs.DirectoryRecord {
-	record, exists := em.m[inodeId]
-	if exists {
-		return record
-	}
-	return nil
-}
-
-func (em *EffectiveMap) del(inodeId InodeId) {
-	delete(em.m, inodeId)
-}
-
-func (em *EffectiveMap) set(inodeId InodeId, record quantumfs.DirectoryRecord) {
-	utils.Assert(em.get(inodeId) == nil, "Setting inodeId for existing record")
-	utils.Assert(record.Type() != quantumfs.ObjectTypeHardlink,
-		"Hardlink records are always publishable.")
-	em.m[inodeId] = record
-}
-
-func (em *EffectiveMap) foreach(fxn func(inodeId InodeId,
-	record quantumfs.DirectoryRecord)) {
-
-	for inodeId, record := range em.m {
-		fxn(inodeId, record)
-	}
-}
-
-func newChildContainer(c *ctx, dir_ *Directory,
+func newChildContainer(c *ctx, dir *Directory,
 	baseLayerId quantumfs.ObjectKey) (*ChildContainer, []InodeId) {
 
-	publishable, uninstantiated := newChildMap(c, dir_, baseLayerId)
-	effective := newEffectiveMap()
-	children := make(map[string]InodeId)
+	container := &ChildContainer{
+		dir:         dir,
+		children:    make(map[string]InodeId),
+		publishable: make(map[InodeId]map[string]quantumfs.DirectoryRecord),
+		effective:   make(map[InodeId]map[string]quantumfs.DirectoryRecord),
+	}
 
-	publishable.foreachChild(c, func(name string, inodeId InodeId) {
-		c.vlog("Adding child %s to the map", name)
-		children[name] = inodeId
+	uninstantiated := container.loadAllChildren(c, baseLayerId)
+
+	return container, uninstantiated
+}
+
+func (container *ChildContainer) loadAllChildren(c *ctx,
+	baseLayerId quantumfs.ObjectKey) []InodeId {
+
+	defer c.funcIn("ChildContainer::loadAllChildren").Out()
+
+	uninstantiated := make([]InodeId, 0, 200) // 200 arbitrarily chosen
+
+	foreachDentry(c, baseLayerId, func(record quantumfs.DirectoryRecord) {
+		c.vlog("Loading child %s", record.Filename())
+		childInodeNum := container.loadChild(c, record,
+			quantumfs.InodeIdInvalid)
+		container.children[record.Filename()] = childInodeNum
+		c.vlog("loaded child %d", childInodeNum)
+		uninstantiated = append(uninstantiated, childInodeNum)
 	})
 
-	return &ChildContainer{
-		dir:         dir_,
-		children:    children,
-		publishable: *publishable,
-		effective:   *effective,
-	}, uninstantiated
+	return uninstantiated
+}
+
+func (container *ChildContainer) loadChild(c *ctx, record quantumfs.DirectoryRecord
+	) InodeId {
+
+	defer c.FuncIn("ChildContainer::loadChild", "inode %d", inodeId).Out()
+
+	// Since we do not have an inodeId this child is/will not be instantiated and
+	// so it placed in the publishable set.
+	
+	inodeId = c.qfs.newInodeId()
+
+	names, exists := container.publishable[inodeId]
+	if !exists {
+		names = make(map[string]quantumfs.DirectoryRecord)
+	}
+
+	names[record.Filename()] = record
+
+	container.publishable[inodeId] = names
+
+	container.children[record.Filename()] = inodeId
+
+	return inodeId
 }
 
 func (container *ChildContainer) setRecord(c *ctx, inodeId InodeId,
 	record quantumfs.DirectoryRecord) {
 
-	container.publishable.setRecord(c, inodeId, record)
+	// Since we have an inodeId this child is or will be instantiated and so is
+	// placed in the effective set.
+
+	names, exists := container.effective[inodeId]
+	if !exists {
+		names = make(map[string]quantumfs.DirectoryRecord)
+	}
+
+	names[record.Filename()] = record
+
+	container.effective[inodeId] = names
+
+	// Build the hardlink path list if we just set a hardlink record
+	if record.Type() == quantumfs.ObjectTypeHardlink {
+		container.dir.markHardlinkPath(c, record.Filename(), record.FileId())
+	}
 }
 
-func (container *ChildContainer) getRecord(c *ctx, inodeId InodeId,
+func (container *ChildContainer) delRecord(c *ctx, inodeId InodeId,
 	name string) quantumfs.DirectoryRecord {
 
-	defer c.FuncIn("ChildContainer::getRecord", "%d %s", inodeId, name).Out()
-	if record := container.effective.get(inodeId); record != nil {
+	record := container.getRecordByName(c, name)
+	if record == nil {
+		return nil // Doesn't exist
+	}
+
+	delete(container.publishable[inodeId], name)
+	delete(container.effective[inodeId], name)
+	delete(container.children, name)
+
+	return record
+}
+
+func (container *ChildContainer) getRecordByName(c *ctx,
+	name string) quantumfs.DirectoryRecord {
+
+	defer c.FuncIn("ChildContainer::getRecordByName", "%s", name).Out()
+
+	inodeId := container.inodeNum(name)
+	records := container.effective[inodeId]
+	if records == nil {
+		records = container.publishable[inodeId]
+	}
+
+	if records == nil {
+		return nil // Does not exist
+	}
+	return records[name]
+}
+
+// Note that this will return one arbitrary record in cases where that inode has
+// multiple names in this container/directory.
+func (container *ChildContainer) getRecordById(c *ctx,
+	inodeId InodeId) quantumfs.DirectoryRecord {
+
+	records := container.effective[inodeId]
+	if records == nil {
+		records = container.publishable[inodeId]
+	}
+
+	if records == nil {
+		return nil // Does not exist
+	}
+	for _, record := range records {
 		return record
 	}
-	return container.publishable.getRecord(c, inodeId, name)
+	utils.Assert(false, "Empty records listing")
 }
 
-func (container *ChildContainer) loadChild(c *ctx, record quantumfs.DirectoryRecord,
-	inodeId InodeId) InodeId {
-
-	defer c.FuncIn("ChildContainer::loadChild", "inode %d", inodeId).Out()
-	if record.Type() == quantumfs.ObjectTypeHardlink {
-		return container.loadPublishableChild(c, record, inodeId)
-	}
-	if inodeId != quantumfs.InodeIdInvalid {
-		container.effective.set(inodeId, record)
-	} else {
-		// As there is no inodeNum, there doesn't exist an instantiated inode
-		// for this child so its entry must be made publishable immediately.
-		inodeId = c.qfs.newInodeId()
-		container.publishable.loadChild(c, record, inodeId)
+func (container *ChildContainer) inodeNum(name string) InodeId {
+	if inodeId, exists := container.children[name]; exists {
+		return inodeId
 	}
 
-	container.children[record.Filename()] = inodeId
-
-	return inodeId
-}
-
-func (container *ChildContainer) loadPublishableChild(c *ctx,
-	record quantumfs.DirectoryRecord, inodeId InodeId) InodeId {
-
-	defer c.FuncIn("ChildContainer::loadPublishableChild", "inode %d",
-		inodeId).Out()
-	inodeId = container.publishable.loadChild(c, record, inodeId)
-	container.children[record.Filename()] = inodeId
-	container.effective.del(inodeId)
-	return inodeId
+	return quantumfs.InodeIdInvalid
 }
 
 func (container *ChildContainer) count() uint64 {
@@ -152,19 +185,44 @@ func (container *ChildContainer) deleteChild(c *ctx, name string,
 
 	defer c.FuncIn("ChildContainer::deleteChild", "name %s", name).Out()
 
-	inodeId := container.children[name]
+	inodeId := container.inodeNum(name)
 	if inodeId == quantumfs.InodeIdInvalid {
 		c.vlog("name %s does not exist", name)
 		return nil
 	}
-	delete(container.children, name)
-	effectiveRecord := container.effective.get(inodeId)
-	container.effective.del(inodeId)
-	record := container.publishable.deleteChild(c, inodeId, name, fixHardlinks)
-	if effectiveRecord != nil {
-		return effectiveRecord
+	record := container.getRecordByName(name)
+
+	// This may be a hardlink that is due to be converted.
+	if hardlink, isHardlink := record.(*Hardlink); isHardlink && fixHardlinks {
+		newRecord, inodeId := container.dir.hardlinkTable.removeHardlink(c,
+			hardlink.fileId)
+
+		// Wsr says we're about to orphan the last hardlink copy
+		if newRecord != nil {
+			newRecord.SetFilename(hardlink.Filename())
+			record = newRecord
+			container.setRecord(c, newRecord, inodeId)
+		}
 	}
-	return record
+
+	result := container.delRecord(c, inodeId, name)
+
+	if link, isHardlink := record.(*Hardlink); isHardlink {
+		if !fixHardlinks {
+			return nil
+		}
+		if !cmap.dir.hardlinkTable.hardlinkExists(c, link.fileId) {
+			c.vlog("hardlink does not exist")
+			return nil
+		}
+		if cmap.dir.hardlinkTable.hardlinkDec(link.fileId) {
+			// If the refcount was greater than one we shouldn't
+			// reparent.
+			c.vlog("Hardlink referenced elsewhere")
+			return nil
+		}
+	}
+	return result
 }
 
 func (container *ChildContainer) renameChild(c *ctx, oldName string,
@@ -172,13 +230,15 @@ func (container *ChildContainer) renameChild(c *ctx, oldName string,
 
 	defer c.FuncIn("ChildContainer::renameChild", "%s -> %s",
 		oldName, newName).Out()
+
 	if oldName == newName {
 		c.vlog("Names are identical")
 		return
 	}
+
 	inodeId := container.inodeNum(oldName)
 	c.vlog("child %s has inode %d", oldName, inodeId)
-	record := container.getRecord(c, inodeId, oldName)
+	record := container.getRecordByName(c, oldName)
 	if record == nil {
 		c.vlog("oldName doesn't exist")
 		return
@@ -193,17 +253,8 @@ func (container *ChildContainer) renameChild(c *ctx, oldName string,
 		hardlink.creationTime = quantumfs.NewTime(time.Now())
 		container.dir.markHardlinkPath(c, record.Filename(), record.FileId())
 	}
-	container.loadPublishableChild(c, record, inodeId)
-
-	delete(container.children, oldName)
-}
-
-func (container *ChildContainer) inodeNum(name string) InodeId {
-	if inodeId, exists := container.children[name]; exists {
-		return inodeId
-	}
-
-	return quantumfs.InodeIdInvalid
+	container.setRecord(c, record, inodeId)
+	container.makePublishable(c, newName)
 }
 
 func (container *ChildContainer) directInodes() []InodeId {
@@ -223,80 +274,113 @@ func (container *ChildContainer) publishableRecords(
 	c *ctx) []quantumfs.DirectoryRecord {
 
 	defer c.funcIn("ChildContainer::publishableRecords").Out()
-	return container.publishable.records()
+
+	records := make([]quantumfs.DirectoryRecord, 0, container.count())
+	for _, byInode := range container.publishable {
+		for _, record := range byInode {
+			records = append(records, record)
+		}
+	}
+
+	return records
 }
 
 func (container *ChildContainer) records() []quantumfs.DirectoryRecord {
 	publishableRecords := container.publishable.records()
 
 	records := make([]quantumfs.DirectoryRecord, 0, container.count())
-	seen := make(map[string]bool, len(records))
+	seen := make(map[string]bool, container.count())
 
-	container.effective.foreach(func(inodeId InodeId,
-		record quantumfs.DirectoryRecord) {
-
-		records = append(records, record)
-		seen[record.Filename()] = true
-	})
-
-	for _, record := range publishableRecords {
-		if _, isSeen := seen[record.Filename()]; isSeen {
-			continue
+	for _, byInode := range container.effective {
+		for _, record := range byInode {
+			records = append(records, record)
+			seen[record.Filename()] = true
 		}
-		records = append(records, record)
 	}
+
+	for _, byInode := range container.publishable {
+		for _, record := range byInode {
+			if !seen[record.Filename()] {
+				records = append(records, record)
+			}
+		}
+	}
+
 	return records
-}
-
-func (container *ChildContainer) record(
-	inodeId InodeId) quantumfs.DirectoryRecord {
-
-	if record := container.effective.get(inodeId); record != nil {
-		return record
-	}
-	return container.publishable.record(inodeId)
-}
-
-func (container *ChildContainer) recordByName(c *ctx,
-	name string) quantumfs.DirectoryRecord {
-
-	defer c.FuncIn("ChildContainer::recordByName", "%s", name).Out()
-	inodeId := container.children[name]
-	if inodeId == quantumfs.InodeIdInvalid {
-		return nil
-	}
-	if record := container.effective.get(inodeId); record != nil {
-		return record
-	}
-	return container.publishable.getRecord(c, inodeId, name)
 }
 
 func (container *ChildContainer) makeHardlink(c *ctx, childId InodeId) (
 	copy quantumfs.DirectoryRecord, err fuse.Status) {
 
 	defer c.FuncIn("ChildContainer::makeHardlink", "inode %d", childId).Out()
-	record := container.record(childId)
-	if record == nil {
+	child := container.getRecordById(childId)
+	if child == nil {
 		c.elog("No child record for inode %d", childId)
 		return nil, fuse.ENOENT
 	}
+
+	// If it's already a hardlink, great no more work is needed
+	if link, isLink := child.(*Hardlink); isLink {
+		c.vlog("Already a hardlink")
+
+		recordCopy := *link
+
+		// Ensure we update the ref count for this hardlink
+		container.dir.hardlinkTable.hardlinkInc(link.fileId)
+
+		return &recordCopy, fuse.OK
+	}
+
+	// record must be a file type to be hardlinked
+	if !child.Type().IsRegularFile() &&
+		child.Type() != quantumfs.ObjectTypeSymlink &&
+		child.Type() != quantumfs.ObjectTypeSpecial {
+
+		c.dlog("Cannot hardlink %s - not a file", record.Filename())
+		return nil, fuse.EINVAL
+	}
+
+	childname := child.Filename()
+	// remove the record from the childmap before donating it to be a hardlink
+	container.delRecord(childId, childname)
+
+	c.vlog("Converting %s into a hardlink", childname)
+	newLink := container.dir.hardlinkTable.newHardlink(c, childId, child)
+
+	linkSrcCopy := newLink.Clone()
+	linkSrcCopy.SetFilename(childname)
+	container.setRecord(c, childId, linkSrcCopy)
+
 	// The child is becoming a hardlink which means it will be part of the
 	// hardlink map in wsr the next time wsr gets published, therefore,
 	// we can mark it as publishable
-	container.deleteChild(c, record.Filename(), false)
-	container.loadPublishableChild(c, record, childId)
-	return container.publishable.makeHardlink(c, childId)
+	container.makePublishable(c, childname)
+
+	newLink.creationTime = quantumfs.NewTime(time.Now())
+	newLink.SetContentTime(newLink.creationTime)
+	return newLink, fuse.OK
 }
 
 func (container *ChildContainer) makePublishable(c *ctx, name string) {
 	defer c.FuncIn("ChildContainer::makePublishable", "%s", name).Out()
 
 	inodeId := container.inodeNum(name)
-	if inodeId == quantumfs.InodeIdInvalid {
-		panic("No such child " + name)
-	}
+	utils.Assert(inodeId != quantumfs.InodeIdInvalid, "No such child %s", name)
 
-	record := container.record(inodeId)
-	container.deleteChild(c, name, false)
-	container.loadPublishableChild(c, record, inodeId)
+	record := container.effective[inodeId][name]
+	utils.Assert(record != nil, "No effective record for name: %s", name)
+
+	records := container.publishable[inodeId]
+	if records == nil {
+		records = make(map[string]quantumfs.DirectoryRecord, 0)
+	}
+	records[name] = record
+	container.publishable[inodeId] = records
+
+	records = container.effective[inodeId]
+	if len(records) == 0 {
+		delete(container.effective, inodeId)
+	} else {
+		delete(container.effective[inodeId], name)
+	}
 }
