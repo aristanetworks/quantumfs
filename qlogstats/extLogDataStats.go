ced5a7d88fda2dddb4fb8fc911494f872c70e9a9
Peter Neufeld <pwn@arista.com>
2018-01-29 11:32:23 -0800

    Review changes

--- qlogstats/extLogDataStats.go
+++ qlogstats/extLogDataStats.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Arista Networks, Inc.  All rights reserved.
+// Copyright (c) 2018 Arista Networks, Inc.  All rights reserved.
 // Arista Networks, Inc. Confidential and Proprietary.
 
 // extLogDataStats is a stat extractor that produces a histogram from log data
@@ -64,7 +64,7 @@ func (ext *extLogDataStats) process() {
 				ext.stats.NewPoint(data)
 			}
 		case PublishCommandType:
-			resultChannel := cmd.Data().(chan PublishResult)
+			resultChannel := cmd.Data().(chan []Measurement)
 			resultChannel <- ext.publish()
 		case GcCommandType:
 			// do nothing since we clear every publish
@@ -72,26 +72,26 @@ func (ext *extLogDataStats) process() {
 	}
 }
 
-func (ext *extLogDataStats) publish() PublishResult {
+func (ext *extLogDataStats) publish() []Measurement {
 	tags := make([]quantumfs.Tag, 0)
-	tags = append(tags, quantumfs.NewTag("statName", ext.name))
+	tags = appendNewTag(tags, "statName", ext.name)
 
 	fields := make([]quantumfs.Field, 0)
 
-	fields = append(fields, quantumfs.NewField("samples", ext.stats.Count()))
+	fields = appendNewFieldInt(fields, "samples", ext.stats.Count())
 
 	for name, data := range ext.stats.Histogram() {
-		fields = append(fields, quantumfs.NewField(name, data))
+		fields = appendNewFieldInt(fields, name, data)
 	}
 
 	// Take note of any parsing errors
-	fields = append(fields, quantumfs.NewField("errors", ext.errors))
+	fields = appendNewFieldInt(fields, "errors", ext.errors)
 
 	ext.stats.Clear()
 	ext.errors = 0
-	return PublishResult{
-		measurement: "quantumFsLogDataStats",
+	return []Measurement{{
+		name: "quantumFsLogDataStats",
 		tags:        tags,
 		fields:      fields,
-	}
+	}}
 }
