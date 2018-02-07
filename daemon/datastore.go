6989cc05e1a68aff1142ad900dd476000e7d986d
Peter Neufeld <pwn@arista.com>
2018-02-07 12:52:15 -0800

    Undo minor change that isn't needed anymore

--- daemon/datastore.go
+++ daemon/datastore.go
@@ -124,7 +124,7 @@ func newEmptyBuffer() buffer {
 
 // Does not obey the initBlockSize capacity, so only for use with buffers that
 // are very unlikely to be written to
-func newBuffer(c *ctx, in []byte, keyType quantumfs.KeyType) *buffer {
+func newBuffer(c *ctx, in []byte, keyType quantumfs.KeyType) quantumfs.Buffer {
 	defer c.FuncIn("newBuffer", "keyType %d", keyType).Out()
 
 	return &buffer{
@@ -136,7 +136,7 @@ func newBuffer(c *ctx, in []byte, keyType quantumfs.KeyType) *buffer {
 }
 
 // Like newBuffer(), but 'in' is copied and ownership is not assumed
-func newBufferCopy(c *ctx, in []byte, keyType quantumfs.KeyType) *buffer {
+func newBufferCopy(c *ctx, in []byte, keyType quantumfs.KeyType) quantumfs.Buffer {
 	inSize := len(in)
 
 	defer c.FuncIn("newBufferCopy", "keyType %d inSize %d", keyType,
