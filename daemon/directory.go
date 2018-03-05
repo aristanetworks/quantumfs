b29ccd88c283d42f0a3db54c0d346c11da1caac8
Travis Brown <travisb@arista.com>
2018-03-05 15:03:24 -0800

    Fix new go vet errors

--- daemon/directory.go
+++ daemon/directory.go
@@ -952,7 +952,6 @@ func (dir *Directory) Rmdir(c *ctx, name string) fuse.Status {
 
 		defer dir.Lock().Unlock()
 
-		var inode InodeId
 		result := func() fuse.Status {
 			defer dir.childRecordLock.Lock().Unlock()
 			record_ := dir.children.recordByName(c, name)
@@ -979,7 +978,7 @@ func (dir *Directory) Rmdir(c *ctx, name string) fuse.Status {
 				return fuse.Status(syscall.ENOTEMPTY)
 			}
 
-			inode = dir.children.inodeNum(name)
+			dir.children.inodeNum(name)
 			return fuse.OK
 		}()
 		if result != fuse.OK {
