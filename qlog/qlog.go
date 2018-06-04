0ebf3fb779e606804450d41684098b295f54b7e8
Peter Neufeld <pwn@arista.com>
2018-06-04 15:26:20 -0700

    Remove unecessary sort type

--- qlog/qlog.go
+++ qlog/qlog.go
@@ -131,17 +131,3 @@ func newLogSubsystem(sys string) LogSubsystem {
 	addLogSubsystem(sys, l)
 	return l
 }
-
-type sortString []string
-
-func (s sortString) Len() int {
-	return len(s)
-}
-
-func (s sortString) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s sortString) Less(i, j int) bool {
-	return s[i] < s[j]
-}
