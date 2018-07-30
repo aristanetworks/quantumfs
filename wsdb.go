7f140c5b28bad238d808b9aeef07fef868e8d5f6
Krishnanand Thommandra <kthommandra@arista.com>
2018-07-30 09:01:33 -0700

    export uncached WSDB API
    Some clients require ether WSDB API endpoint so that ether specific
    WSDB APIs (eg: WorkspaceLastWriteTime) can be used. These APIs
    require uncached access to workspacedb. Ether WSDB cache
    can be expensive depending on cache timeout configuration. So such
    clients can use an uncached endpoint.
    
    Change-Id: I2adf8a57c818328e6d535e8f89a7529f80e4dde7

--- wsdb.go
+++ wsdb.go
@@ -15,9 +15,9 @@ import (
 // may be cache enabled or uncached depending
 // on workspace DB configuration parameter.
 
-// NewWorkspaceDB creates a workspace DB API endpoint
-func NewWorkspaceDB(confName string) wsdb.WorkspaceDB {
-
+// NewUncachedWorkspaceDB creates an uncached
+// workspace DB API endpoint.
+func NewUncachedWorkspaceDB(confName string) wsdb.WorkspaceDB {
 	cfg, err := readCqlConfig(confName)
 	if err != nil {
 		fmt.Println("Error reading CQL config: ", err)
@@ -28,7 +28,21 @@ func NewWorkspaceDB(confName string) wsdb.WorkspaceDB {
 	if wsdb, err = newNoCacheWsdb(cluster, cfg); err != nil {
 		panic(fmt.Sprintf("Error %q during NewNoCacheWsdb", err))
 	}
+	return wsdb
+}
 
+// NewWorkspaceDB creates a cached workspace DB API endpoint.
+// This endpoint caches objects like
+// typespaces, namespaces, workspaces etc. The cache
+// can be tuned using WSDB section in config file.
+func NewWorkspaceDB(confName string) wsdb.WorkspaceDB {
+	cfg, err := readCqlConfig(confName)
+	if err != nil {
+		fmt.Println("Error reading CQL config: ", err)
+		panic(err.Error())
+	}
+
+	wsdb := NewUncachedWorkspaceDB(confName)
 	return newCacheWsdb(wsdb, cfg.WsDB)
 }
 
