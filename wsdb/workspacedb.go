d50881468e0a3d879ea12c0042fd008d2afcf8e8
Travis Brown <travisb@arista.com>
2018-11-20 23:40:27 +0000

    Return the nonce in the AdvanceWorkspace API
    This is the new API required by http://gut/review/r/1567
    
    Change-Id: I83f9356871e99b2c69837dbaa2e1641a1035fd73

--- wsdb/workspacedb.go
+++ wsdb/workspacedb.go
@@ -190,7 +190,7 @@ type WorkspaceDB interface {
 	//  ErrWorkspaceOutOfDate: The workspace rootID was changed
 	//  	remotely so the local instance is out of date
 	AdvanceWorkspace(c ether.Ctx, typespace string, namespace string, workspace string,
-		nonce WorkspaceNonce, currentRootID ObjectKey, newRootID ObjectKey) (ObjectKey, error)
+		nonce WorkspaceNonce, currentRootID ObjectKey, newRootID ObjectKey) (ObjectKey, WorkspaceNonce, error)
 
 	SetWorkspaceImmutable(c ether.Ctx, typespace string, namespace string,
 		workspace string) error
